3e0700f3de618b12bd67d499598d0fac677c1ce8
Maksim <max.freedman.main@gmail.com>
2021-06-04 14:13:29 +0300

    added annotation to models

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -1,11 +1,27 @@
 package mate.academy.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "movie_sessions")
 public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private CinemaHall cinemaHall;
+
     private LocalDateTime showTime;
 
     public MovieSession() {
