351b6a69f14e49086517b95489f3612f62fee9bd
Bohdan Borsh <borshbohdan@gmail.com>
2021-06-07 15:58:07 +0300

    HW Done. Fetch types removed + fixes

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -2,7 +2,6 @@ package mate.academy.model;
 
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
