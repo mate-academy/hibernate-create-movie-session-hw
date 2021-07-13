b8bd11b41c70ad74df6949a037868f31987afe66
Максим <mbelmasg@gmail.com>
2021-07-13 16:35:10 +0300

    start

---
+++ src/main/java/mate/academy/dao/MovieSessionDao.java
@@ -0,0 +1,15 @@
+package mate.academy.dao;
+
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface MovieSessionDao {
+
+    MovieSession add(MovieSession movieSession);
+
+    MovieSession get(Long id);
+
+    List<MovieSession> findAvailableSessions(Long movieId, LocalDate date);
+}
---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import java.time.LocalDate;
+import java.util.List;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        return null;
+    }
+
+    @Override
+    public MovieSession get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        return null;
+    }
+}
