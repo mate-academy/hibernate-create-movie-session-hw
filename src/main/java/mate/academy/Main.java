6921afd80a6982245c4031e3f0fa2b5842cbae8a
Denis <deniskichan@gmail.com>
2021-08-03 16:50:28 +0300

    edited annotations

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,60 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService = null;
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
+        Movie shrek = new Movie("Shrek");
+        shrek.setDescription("Angry ogre");
+        movieService.add(shrek);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        CinemaHall redHall = new CinemaHall();
+        redHall.setCapacity(30);
+        redHall.setDescription("Red Hall");
+        cinemaHallService.add(redHall);
+
+        CinemaHall blueHall = new CinemaHall();
+        blueHall.setCapacity(50);
+        blueHall.setDescription("Blue Hall");
+        cinemaHallService.add(blueHall);
+
+        System.out.println(cinemaHallService.get(redHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+
+        MovieSession movieSession1 = new MovieSession();
+        movieSession1.setMovie(shrek);
+        movieSession1.setCinemaHall(redHall);
+        movieSession1.setShowTime(LocalDateTime.now());
+        MovieSessionService movieSessionService =
+                (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession1);
+
+        MovieSession movieSession2 = new MovieSession();
+        movieSession2.setMovie(fastAndFurious);
+        movieSession2.setCinemaHall(blueHall);
+        movieSession2.setShowTime(LocalDateTime.now().minusDays(1));
+        movieSessionService.add(movieSession2);
+
+        movieSessionService.findAvailableSessions(2L, LocalDate.now())
+                .forEach(System.out::println);
     }
 }
