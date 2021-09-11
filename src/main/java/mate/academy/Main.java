3c66f4144280fcd062d704a52105500ab42f592e
Vitalii Martynov <VitaliiMvDeveloper@gmail.com>
2021-09-11 23:19:33 +0300

    Initial solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,90 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
+    private static final MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+
+    private static final CinemaHallService cinemaHallService = (CinemaHallService) injector
+            .getInstance(CinemaHallService.class);
+
+    private static final MovieSessionService movieSessionService = (MovieSessionService) injector
+            .getInstance(MovieSessionService.class);
+
+    public static void main(String[] args) {
+        initializeMovies();
+        initializeCinemaHalls();
+        initializeMovieSessions();
+
+        System.out.println("============================");
+        movieService.getAll().forEach(System.out::println);
+        System.out.println("============================");
+        cinemaHallService.getAll().forEach(System.out::println);
+        System.out.println("============================");
+        movieSessionService.findAvailableSessions(1L, LocalDate.of(2021, 9, 12))
+                .forEach(System.out::println);
+
+    }
+
+    private static void initializeMovies() {
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
+
+        Movie movieTerminator = new Movie("The Terminator");
+        movieTerminator.setDescription("Action with Arnold Schwarzenegger. 1984");
+        movieService.add(movieTerminator);
+
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+    }
+
+    private static void initializeCinemaHalls() {
+        CinemaHall yellow = new CinemaHall(20, "Yellow hall");
+        cinemaHallService.add(yellow);
+
+        CinemaHall green = new CinemaHall(80, "Green hall");
+        cinemaHallService.add(green);
+    }
+
+    private static void initializeMovieSessions() {
+        MovieSession msFastAndFurious1 = new MovieSession(movieService.get(1L),
+                cinemaHallService.get(1L), LocalDateTime.of(2021, 9, 12, 9, 0));
+        movieSessionService.add(msFastAndFurious1);
+
+        MovieSession msFastAndFurious2 = new MovieSession(movieService.get(1L),
+                cinemaHallService.get(1L), LocalDateTime.of(2021, 9, 12, 12, 0));
+        movieSessionService.add(msFastAndFurious2);
+
+        MovieSession msFastAndFurious3 = new MovieSession(movieService.get(1L),
+                cinemaHallService.get(2L), LocalDateTime.of(2021, 9, 12, 18, 0));
+        movieSessionService.add(msFastAndFurious3);
+
+        MovieSession msFastAndFurious4 = new MovieSession(movieService.get(1L),
+                cinemaHallService.get(1L), LocalDateTime.of(2021, 9, 14, 9, 0));
+        movieSessionService.add(msFastAndFurious4);
+
+        MovieSession msFastAndFurious5 = new MovieSession(movieService.get(1L),
+                cinemaHallService.get(2L), LocalDateTime.of(2021, 9, 14, 9, 0));
+        movieSessionService.add(msFastAndFurious5);
+
+        MovieSession msTerminator1 = new MovieSession(movieService.get(2L),
+                cinemaHallService.get(2L), LocalDateTime.of(2021, 9, 12, 9, 0));
+        movieSessionService.add(msTerminator1);
+
+        MovieSession msTerminator2 = new MovieSession(movieService.get(2L),
+                cinemaHallService.get(1L), LocalDateTime.of(2021, 9, 12, 18, 0));
+        movieSessionService.add(msTerminator2);
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,68 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movie session " + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<MovieSession> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(MovieSession.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a movie session by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> getMovieSessionQuery = session.createQuery("FROM MovieSession ms "
+                    + "LEFT JOIN FETCH ms.cinemaHall "
+                    + "WHERE ms.movie.id = :movieId AND ms.showTime BETWEEN :startDT AND :endDT",
+                    MovieSession.class);
+            getMovieSessionQuery.setParameter("movieId", movieId);
+            getMovieSessionQuery.setParameter("startDT",
+                    LocalDateTime.of(date, LocalTime.of(0,0,0)));
+            getMovieSessionQuery.setParameter("endDT",
+                    LocalDateTime.of(date, LocalTime.of(23,59,59)));
+            return getMovieSessionQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Couldn't find movie sessions on " + date
+                    + " by movie id: " + movieId, e);
+        }
+    }
+}
