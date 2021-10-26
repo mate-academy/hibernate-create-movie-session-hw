3d792cf83254ee9b9850ce0a1cc7bcc30d9aae5e
Andrii Demchenko <avd998ne@gmail.com>
2021-10-26 21:20:16 +0300

    added Dao and Service interfaces and classes

---
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,22 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.model.CinemaHall;
+
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        return null;
+    }
+
+    @Override
+    public CinemaHall get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        return null;
+    }
+}
---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,24 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.model.MovieSession;
+
+public class MovieSessionDaoImpl implements MovieSessionDao {
+
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        return null;
+    }
+
+    @Override
+    public MovieSession get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        return null;
+    }
+}
