9b63d628ecf2101bfd25094277714e076a5b8cc5
Sasha Hordenin <sg743148@gmail.com>
2021-10-26 19:50:20 +0300

    correction of exception text

--- src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -27,7 +27,7 @@ public class CinemaHallDaoImpl implements CinemaHallDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert cinemaHall " + cinemaHall, e);
+            throw new DataProcessingException("Can't insert cinema hall " + cinemaHall, e);
         } finally {
             if (session != null) {
                 session.close();
