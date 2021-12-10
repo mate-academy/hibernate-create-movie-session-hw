4c91dcfb9c6fae322f83ea2046ec4cf448fbf4e6
DmytroPetrushenko <dmytro.petrushenko@gmail.com>
2021-12-10 16:30:21 +0200

    models done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,9 +2,14 @@ package mate.academy;
 
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        /*
         MovieService movieService = null;
 
         Movie fastAndFurious = new Movie("Fast and Furious");
@@ -12,5 +17,7 @@ public class Main {
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+         */
     }
 }
