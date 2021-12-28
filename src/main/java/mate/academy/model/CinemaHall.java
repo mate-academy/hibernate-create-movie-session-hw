c39f9f86150b774e3810003969ac11251b542ce7
Oleksii-kho <oleksiikhoriev@gmail.com>
2021-12-28 12:59:16 +0200

    changed getter and setter

--- src/main/java/mate/academy/model/CinemaHall.java
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -23,11 +23,11 @@ public class CinemaHall {
         this.description = description;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
