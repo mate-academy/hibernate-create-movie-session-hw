5e25b3a4ff40c3ade9ac1a8407f2c9d3fcfb7470
ASelivonchyk <aselivonchyk@gmail.com>
2022-01-14 16:30:45 +0200

    Wrote an assignment hibernate-create-movie-session-hw. - configure hibernate.cfg.xml; - created POJO CinemaHall, MovieSession; - created CinemaHallDao, MovieSessionDao with its impl; - implement getAll() method to MovieDao and its impl.

--- src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -2,54 +2,55 @@ package mate.academy.dao.impl;
 
 import java.util.List;
 import java.util.Optional;
-import mate.academy.dao.MovieDao;
+import mate.academy.dao.CinemaHallDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
-import mate.academy.model.Movie;
+import mate.academy.model.CinemaHall;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Dao
-public class MovieDaoImpl extends AbstractDaoImpl implements MovieDao {
-
+public class CinemaHallDaoImpl extends AbstractDaoImpl implements CinemaHallDao {
     @Override
-    public Movie add(Movie movie) {
-        Transaction transaction = null;
+    public CinemaHall add(CinemaHall cinemaHall) {
         Session session = null;
+        Transaction transaction = null;
         try {
             session = sessionFactory.openSession();
             transaction = session.beginTransaction();
-            session.save(movie);
+            session.save(cinemaHall);
             transaction.commit();
-            return movie;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert movie " + movie, e);
+            throw new DataProcessingException(
+                    "Can't add cinema-hall: " + cinemaHall + " to Database", e);
         } finally {
             if (session != null) {
                 session.close();
             }
         }
+        return cinemaHall;
     }
 
     @Override
-    public Optional<Movie> get(Long id) {
+    public Optional<CinemaHall> get(Long id) {
         try (Session session = sessionFactory.openSession()) {
-            return Optional.ofNullable(session.get(Movie.class, id));
+            return Optional.ofNullable(session.get(CinemaHall.class, id));
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get a movie by id: " + id, e);
+            throw new DataProcessingException(
+                    "Can't get a cinema-hall by id: " + id + " from Database", e);
         }
     }
 
     @Override
-    public List<Movie> getAll() {
+    public List<CinemaHall> getAll() {
         try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM Movie", Movie.class)
+            return session.createQuery("FROM CinemaHall", CinemaHall.class)
                           .getResultList();
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get a movies from Database", e);
+            throw new DataProcessingException("Can't get cinema-halls from Database", e);
         }
     }
 }
