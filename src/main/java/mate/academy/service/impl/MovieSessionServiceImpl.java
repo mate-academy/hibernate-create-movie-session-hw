e2201027965175e54faf698cdb913cc5e6e654c7
Oleh Mazurkov <o.mazurkov@gmail.com>
2022-02-02 18:02:08 +0200

    solution hibernate-create-movie-session-hw

--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -2,7 +2,6 @@ package mate.academy.service.impl;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import mate.academy.dao.MovieSessionDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
@@ -20,8 +19,8 @@ public class MovieSessionServiceImpl implements MovieSessionService {
     }
 
     @Override
-    public Optional<MovieSession> get(Long id) {
-        return movieSessionDao.get(id);
+    public MovieSession get(Long id) {
+        return movieSessionDao.get(id).get();
     }
 
     @Override
