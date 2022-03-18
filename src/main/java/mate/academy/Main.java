288d7497ff87e551af165abbf246f53e1493c898
Sasha Bachynskyi <allfred12@gmail.com>
2022-03-18 14:31:48 +0200

    crated model MovieSession, implemented MovieSessionDao, MovieSessionService and their methods

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -3,8 +3,13 @@ package mate.academy;
 import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
@@ -24,5 +29,21 @@ public class Main {
         cinemaHallService.add(redHall);
         System.out.println(cinemaHallService.get(redHall.getId()));
         cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSessionService movieSessionService = (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        MovieSession movieSessionOnFriday = new MovieSession();
+        movieSessionOnFriday.setMovie(fastAndFurious);
+        movieSessionOnFriday.setCinemaHall(redHall);
+        movieSessionOnFriday.setShowTime(LocalDateTime.of(2022, 3, 25, 20, 0));
+        System.out.println(movieSessionService.add(movieSessionOnFriday));
+        System.out.println(movieSessionService.get(movieSessionOnFriday.getId()));
+        MovieSession movieSessionOnMonday = new MovieSession();
+        movieSessionOnMonday.setMovie(fastAndFurious);
+        movieSessionOnMonday.setCinemaHall(redHall);
+        movieSessionOnMonday.setShowTime(LocalDateTime.of(2022, 3, 21, 15, 0));
+        System.out.println(movieSessionService.add(movieSessionOnMonday));
+        System.out.println(movieSessionService.get(movieSessionOnMonday.getId()));
+        System.out.println(System.lineSeparator());
+        System.out.println(movieSessionService.findAvailableSessions(1L, LocalDate.of(2022, 3, 25)));
     }
 }
