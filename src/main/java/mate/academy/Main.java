a6672da1d05ee01c3b3d7e5ba26a689f6b375cca
Sasha Bachynskyi <allfred12@gmail.com>
2022-03-18 12:27:23 +0200

    created model CinemaHall, implemented CinemaHallDao, CinemaHallService and their methods

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,9 @@
 package mate.academy;
 
 import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 
 public class Main {
@@ -9,11 +11,18 @@ public class Main {
 
     public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHallService cinemaHallService = (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        CinemaHall redHall = new CinemaHall();
+        redHall.setCapacity(55);
+        redHall.setDescription("large hall with soft armchairs");
+        cinemaHallService.add(redHall);
+        System.out.println(cinemaHallService.get(redHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
     }
 }
