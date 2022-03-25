e52e550405ce60db55b94c3c3652b45d2103812d
nastyaprokhorenko <nastyaprokhorenko527@gmail.com>
2022-03-25 21:11:00 +0200

    Models of CinemaHall and MovieSession were added

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,59 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
+    public static void main(String[] args) {
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall vipCinemaHall = new CinemaHall();
+        vipCinemaHall.setCapacity(100);
+        vipCinemaHall.setDescription("VIP hall");
+        CinemaHallService cinemaHallService
+                = (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        cinemaHallService.add(vipCinemaHall);
+        System.out.println(cinemaHallService.get(vipCinemaHall.getId()));
+
+        CinemaHall threeDCinemaHall = new CinemaHall();
+        threeDCinemaHall.setCapacity(200);
+        threeDCinemaHall.setDescription("3D hall");
+        cinemaHallService.add(threeDCinemaHall);
+        System.out.println(cinemaHallService.get(threeDCinemaHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSession sessionInVipHall = new MovieSession();
+        sessionInVipHall.setMovie(fastAndFurious);
+        sessionInVipHall.setCinemaHall(vipCinemaHall);
+        sessionInVipHall.setShowTime(LocalDateTime
+                .of(2022, 1, 22, 20, 30));
+        MovieSessionService movieSessionService
+                = (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(sessionInVipHall);
+        System.out.println(movieSessionService.get(sessionInVipHall.getId()));
+
+        MovieSession sessionInThreeDHall = new MovieSession();
+        sessionInThreeDHall.setMovie(fastAndFurious);
+        sessionInThreeDHall.setCinemaHall(threeDCinemaHall);
+        sessionInThreeDHall.setShowTime(LocalDateTime
+                .of(2022, 2, 22, 18, 20));
+        movieSessionService.add(sessionInThreeDHall);
+        System.out.println(movieSessionService.get(sessionInThreeDHall.getId()));
+        movieSessionService.findAvailableSessions(fastAndFurious.getId(),
+                LocalDate.of(2022, 1, 22)).forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,63 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movie session " + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<MovieSession> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(MovieSession.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a cinema hall by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> availableSession = session.createQuery("from MovieSession "
+                    + "where showTime >= :startDate and showTime < :endDate "
+                    + "and movie.id = :movie", MovieSession.class);
+            availableSession.setParameter("startDate", date.atStartOfDay());
+            availableSession.setParameter("endDate", date.atStartOfDay().plusDays(1));
+            availableSession.setParameter("movie", movieId);
+            return availableSession.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find movie session with movie id " + movieId
+                    + " and date " + date, e);
+        }
+    }
+}
