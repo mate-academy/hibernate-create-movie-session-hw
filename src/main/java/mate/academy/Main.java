301b629510650e7e091dadca65dfbe0ce9eab00c
Maksym Pokras <mxpokras@gmail.com>
2022-03-29 15:54:01 +0200

    Main.main done

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -3,9 +3,14 @@ package mate.academy;
 import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     private final static Injector injector = Injector.getInstance("mate.academy");
@@ -21,6 +26,10 @@ public class Main {
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
+        Movie snatch = new Movie("Snatch");
+        snatch.setDescription("An action film about world of english crimes");
+        movieService.add(snatch);
+        System.out.println(movieService.get(snatch.getId()));
         movieService.getAll().forEach(System.out::println);
         CinemaHall yellowCinemaHall = new CinemaHall();
         yellowCinemaHall.setDescription("Yellow");
@@ -33,6 +42,57 @@ public class Main {
         cinemaHallService.add(blueCinemaHall);
         System.out.println(cinemaHallService.get(blueCinemaHall.getId()));
         System.out.println(cinemaHallService.getAll());
+        LocalDateTime showTime = LocalDate.now().atStartOfDay();
+        movieSessionService.add(genMovieSession(fastAndFurious, yellowCinemaHall,
+                showTime.plusHours(9)));
+        movieSessionService.add(genMovieSession(fastAndFurious, blueCinemaHall,
+                showTime.plusHours(10)));
+        movieSessionService.add(genMovieSession(fastAndFurious, yellowCinemaHall,
+                showTime.plusHours(12)));
+        movieSessionService.add(genMovieSession(fastAndFurious, blueCinemaHall,
+                showTime.plusHours(13)));
+        movieSessionService.add(genMovieSession(snatch, yellowCinemaHall,
+                showTime.plusHours(15)));
+        movieSessionService.add(genMovieSession(snatch, blueCinemaHall,
+                showTime.plusHours(16)));
+        movieSessionService.add(genMovieSession(snatch, yellowCinemaHall,
+                showTime.plusHours(18)));
+        movieSessionService.add(genMovieSession(snatch, blueCinemaHall,
+                showTime.plusHours(19)));
+        showTime = showTime.toLocalDate().atStartOfDay().plusDays(1);
+        movieSessionService.add(genMovieSession(snatch, yellowCinemaHall,
+                showTime.plusHours(9)));
+        movieSessionService.add(genMovieSession(snatch, blueCinemaHall,
+                showTime.plusHours(10)));
+        movieSessionService.add(genMovieSession(snatch, yellowCinemaHall,
+                showTime.plusHours(12)));
+        movieSessionService.add(genMovieSession(snatch, blueCinemaHall,
+                showTime.plusHours(13)));
+        movieSessionService.add(genMovieSession(fastAndFurious, yellowCinemaHall,
+                showTime.plusHours(15)));
+        movieSessionService.add(genMovieSession(fastAndFurious, blueCinemaHall,
+                showTime.plusHours(16)));
+        movieSessionService.add(genMovieSession(fastAndFurious, yellowCinemaHall,
+                showTime.plusHours(18)));
+        movieSessionService.add(genMovieSession(fastAndFurious, blueCinemaHall,
+                showTime.plusHours(19)));
+        System.out.println(movieSessionService.findAvailableSessions(snatch.getId(),
+                showTime.toLocalDate()));
+        System.out.println(movieSessionService.findAvailableSessions(fastAndFurious.getId(),
+                showTime.toLocalDate()));
+        showTime = showTime.minusDays(1);
+        System.out.println(movieSessionService.findAvailableSessions(snatch.getId(),
+                showTime.toLocalDate()));
+        System.out.println(movieSessionService.findAvailableSessions(fastAndFurious.getId(),
+                showTime.toLocalDate()));
+    }
 
+    private static MovieSession genMovieSession(Movie movie, CinemaHall hall,
+                                                LocalDateTime showTime) {
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(movie);
+        movieSession.setCinemaHall(hall);
+        movieSession.setShowTime(showTime);
+        return movieSession;
     }
 }
