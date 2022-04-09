9a1a96066067dd1830c25cf99ade8f7e1cc1ed83
AprioriKS <sounddummies@gmail.com>
2022-04-09 14:48:47 +0300

    add some class

---
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -0,0 +1,48 @@
+package mate.academy.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.ManyToOne;
+
+public class MovieSession {
+    private Long id;
+    @ManyToOne
+    private Movie movie;
+    @ManyToOne
+    private CinemaHall cinemaHall;
+    private LocalDateTime showTime;
+
+    public MovieSession() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public CinemaHall getCinemaHall() {
+        return cinemaHall;
+    }
+
+    public void setCinemaHall(CinemaHall cinemaHall) {
+        this.cinemaHall = cinemaHall;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+}
