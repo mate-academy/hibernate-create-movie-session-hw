4623d46333c60b9cab45a32aa7bd0ff8b131e354
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-04-11 16:22:13 +0300

    Removed redunant default constructors and changed access modifier of injector in main class into final

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,7 +11,7 @@ import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("mate.academy");
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
         final MovieService movieService =
