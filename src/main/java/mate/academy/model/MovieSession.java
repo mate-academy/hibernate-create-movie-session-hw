9f09cfad5e93d625d453a111835f42ac7a533ced
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-05-31 09:55:25 +0300

    fix mistake part 2

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -1,13 +1,7 @@
 package mate.academy.model;
 
 import java.time.LocalDateTime;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "movies_sessions")
@@ -21,7 +15,7 @@ public class MovieSession {
     @ManyToOne
     @JoinColumn(name = "cinema_hall_id")
     private CinemaHall cinemaHall;
-    @JoinColumn(name = "show_time")
+    @Column(name = "show_time")
     private LocalDateTime showTime;
 
     public MovieSession() {
