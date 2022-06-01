a96a074760ba8c74d1527d54b18583c6d7250069
Vova Klimyuk <klimyuk054@gmail.com>
2022-06-01 20:17:48 +0300

    added CinemaHall, MovieSession, implemented dao and service layer for new models

---
+++ src/main/java/mate/academy/exception/ServiceProcessingException.java
@@ -0,0 +1,8 @@
+package mate.academy.exception;
+
+public class ServiceProcessingException extends RuntimeException {
+
+    public ServiceProcessingException(String message) {
+        super(message);
+    }
+}
