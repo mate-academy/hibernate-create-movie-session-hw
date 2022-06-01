b92c9454ad2f7f025cd892afcb3839f57f21bec7
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-06-01 22:26:57 +0300

    empty line in cfg typo fix

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -20,7 +20,7 @@ public class CinemaHallServiceImpl implements CinemaHallService {
     @Override
     public CinemaHall get(Long id) {
         return cinemaHallDao.get(id).orElseThrow(
-                () -> new RuntimeException("Cant get cinema hall by id: " + id));
+                () -> new RuntimeException("Can't get cinema hall by id: " + id));
     }
 
     @Override
