249c74c28ce7a09a0afa8f2766b209422206edfe
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-06-01 20:19:49 +0200

    dao and models were added

---
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -0,0 +1,24 @@
+package mate.academy.service.impl;
+
+import mate.academy.model.MovieSession;
+import mate.academy.service.MovieSessionService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class MovieSessionServiceImpl implements MovieSessionService {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        return null;
+    }
+
+    @Override
+    public MovieSession get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        return null;
+    }
+}
