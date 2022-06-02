b2ec2f64e4b5131e2d0d6b58dfb9dc12982cc927
Vova Klimyuk <klimyuk054@gmail.com>
2022-06-02 11:47:56 +0300

    fixed mentor's remarks

--- src/main/java/mate/academy/exception/ServiceProcessingException.java
+++ src/main/java/mate/academy/exception/ServiceProcessingException.java
@@ -1,7 +1,6 @@
 package mate.academy.exception;
 
 public class ServiceProcessingException extends RuntimeException {
-
     public ServiceProcessingException(String message) {
         super(message);
     }
