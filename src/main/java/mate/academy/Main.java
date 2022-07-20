33a728170ee2c8e1b67ef1b96eb16411bae9651c
Mykola Yakymiv <mykola8426@gmail.com>
2022-07-20 16:02:46 +0300

    added fixes after review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -12,8 +12,10 @@ import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
 
 public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        Injector injector = Injector.getInstance("mate.academy");
         MovieService movieService =
                 (MovieService) injector.getInstance(MovieService.class);
 
