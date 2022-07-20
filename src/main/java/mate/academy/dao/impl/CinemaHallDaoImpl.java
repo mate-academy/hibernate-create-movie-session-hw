a40a2c42bbb016190d37d246409f6e96059cc601
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-07-20 13:49:05 +0300

    CinemaHallDaoImpl and CinemaHallServiceImpl were implemented

---
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,64 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.CinemaHall;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    private SessionFactory sessionFactory;
+
+    public CinemaHallDaoImpl() {
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(cinemaHall);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return cinemaHall;
+    }
+
+    @Override
+    public Optional<CinemaHall> get(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.get(CinemaHall.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Cannot get CinemaHall entity by ID="
+                    + id + " from DB", e);
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<CinemaHall> allCinemaQuery = session
+                    .createQuery("from cinemaHalls", CinemaHall.class);
+            return allCinemaQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Cannot get list of CinemaHall from DB", e);
+        }
+    }
+}
