c653d2c4eb9d40ab6538a75c4d91f4553ce57f87
Oleksii Mospan <alexey.mospan@gmail.com>
2022-08-15 04:25:46 +0300

    Changed to use jakarta libs

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -1,13 +1,13 @@
 package mate.academy.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "movies")
+@Table
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
