e5044df0c32c474b1be0f779a37abeacebc2cb08
Vitaliy Zahrenko <vzagorenko@gmail.com>
2022-09-04 15:35:16 +0300

    findAvailableSessions released via HQL

---
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -0,0 +1,67 @@
+package mate.academy.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "moviesessions")
+public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Movie movie;
+    @ManyToOne
+    private CinemaHall cinemaHall;
+    private LocalDateTime sessionTime;
+
+    public MovieSession() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public CinemaHall getCinemahall() {
+        return cinemaHall;
+    }
+
+    public void setCinemahall(CinemaHall cinemaHall) {
+        this.cinemaHall = cinemaHall;
+    }
+
+    public LocalDateTime getSessionTime() {
+        return sessionTime;
+    }
+
+    public void setSessionTime(LocalDateTime sessionTime) {
+        this.sessionTime = sessionTime;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieSession{"
+                + "id=" + id
+                + ", Movie='" + movie + '\''
+                + ", cinemaHall='" + cinemaHall + '\''
+                + ", sessionTime='" + sessionTime + '\''
+                + '}';
+    }
+}
