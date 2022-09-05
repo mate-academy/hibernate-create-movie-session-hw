ba4c04edef3b3c982826ecf32bd2910354062602
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-09-05 19:39:27 +0300

    Add new models

---
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,52 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.model.CinemaHall;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(cinemaHall);
+            transaction.commit();
+            return cinemaHall;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save a cinema hall to DB " + cinemaHall, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public CinemaHall get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(CinemaHall.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get a cinema hall to DB by id " + id, e);
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String query = "FROM CinemaHall";
+            Query<CinemaHall> getAll = session.createQuery(query, CinemaHall.class);
+            return getAll.getResultList();
+        }
+    }
+}
--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -9,6 +9,8 @@ import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
@@ -45,6 +47,10 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public List<Movie> getAll() {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String query = "FROM Movie";
+            Query<Movie> getAll = session.createQuery(query, Movie.class);
+            return getAll.getResultList();
+        }
     }
 }
