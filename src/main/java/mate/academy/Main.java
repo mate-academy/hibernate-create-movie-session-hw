01b7f47c279a8e91f0fc020f8be02713c3cd46d4
Mykola Skrypka <mykolaskrypka@gmail.com>
2022-09-29 15:29:39 +0300

    Table 'movie_session_app.movies' doesn't exist

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,20 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
     public static void main(String[] args) {
-        MovieService movieService = null;
+        //MovieService movieService = null;
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
+        //movieService.getAll().forEach(System.out::println);
     }
 }
