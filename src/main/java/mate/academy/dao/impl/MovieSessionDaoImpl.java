fb65b2860841c99c3a21ae2e8e6e0ed899242f0f
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-09-29 12:20:38 +0300

    Movie Session Realise

---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,66 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movie session " + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<MovieSession> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(MovieSession.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a movie session by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> getAvailableSessions = session.createQuery(
+                    "from MovieSession where movie_id = :value and "
+                            + "showTime between :startSession and :endSession",
+                    MovieSession.class);
+            getAvailableSessions.setParameter("value", movieId);
+            getAvailableSessions.setParameter("startSession", date.atStartOfDay());
+            getAvailableSessions.setParameter("endSession",
+                    LocalDateTime.of(date.getYear(), date.getMonth(),
+                            date.getDayOfMonth(), 23, 59));
+            return getAvailableSessions.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a movie session by id: " + movieId, e);
+        }
+    }
+}
