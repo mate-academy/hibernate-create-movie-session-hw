4e3756eb9bf9d50328b530090315869490c90fb6
Mykola Skrypka <mykolaskrypka@gmail.com>
2022-09-30 12:01:43 +0300

    created CinemaHall and MovieSession and implemented Dao and Service layers;

---
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -0,0 +1,57 @@
+package mate.academy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CinemaHall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private int capacity;
+
+    public CinemaHall() {
+    }
+
+    public CinemaHall(String description, int capacity) {
+        this.description = description;
+        this.capacity = capacity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "CinemaHall{"
+                + "id=" + id
+                + ", description='" + description + '\''
+                + ", capacity=" + capacity
+                + '}';
+    }
+}
