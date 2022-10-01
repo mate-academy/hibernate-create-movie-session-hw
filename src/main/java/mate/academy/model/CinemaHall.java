764218a1ef22aac8743f5483eeda8a3cbb32704e
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-01 10:03:11 +0300

    method getAll in MovieDaoImpl was implemented

--- src/main/java/mate/academy/model/CinemaHall.java
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 @Table(name = "cinema_halls")
 public class CinemaHall {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int capacity;
     private String description;
