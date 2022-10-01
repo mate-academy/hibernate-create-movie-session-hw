bb32ca5f7304bf0d1f266a577e7fec38204aadd5
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-01 17:44:34 +0300

    method findAvailableSession was implemented in MovieSessionDaiImpl

--- src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -1,7 +1,7 @@
 package mate.academy.dao.impl;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 import mate.academy.dao.MovieSessionDao;
@@ -11,6 +11,7 @@ import mate.academy.model.MovieSession;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 @Dao
 public class MovieSessionDaoImpl implements MovieSessionDao {
@@ -47,6 +48,17 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
 
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        return new ArrayList<>();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> query = session.createQuery("FROM MovieSession "
+                    + "WHERE Movie.id = :id "
+                    + "AND showTime BETWEEN :startOfDay AND :endOfDay", MovieSession.class);
+            query.setParameter("id", movieId);
+            query.setParameter("startOfDay", LocalTime.MIN.atDate(date));
+            query.setParameter("endOfDay", LocalTime.MAX.atDate(date));
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find a MovieSession for movie with id="
+                    + movieId + " and date " + date, e);
+        }
     }
 }
