8b2216f0049a016e7e6ce4c6fbfa5f23dab24fdc
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-01 20:33:45 +0200

    add constructor to entity class

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -15,6 +15,9 @@ public class Movie {
     private String title;
     private String description;
 
+    public Movie() {
+    }
+
     public Movie(String title) {
         this.title = title;
     }
