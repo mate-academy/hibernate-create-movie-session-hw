ef31bb0f186e5aa9bfa6013847d2fc4e5daa984b
fliarkovskyi <fliarkovskyi@gmail.com>
2022-10-03 14:02:59 +0300

    fix date

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -21,52 +21,52 @@ public class Main {
             = (CinemaHallService) injector.getInstance(CinemaHallService.class);
 
     public static void main(String[] args) {
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing");
-        Movie harryPotter = new Movie("Harry Potter");
-        harryPotter.setDescription("Is a series fantasy films");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.add(harryPotter);
+        Movie black_adam = new Movie("Black Adam");
+        black_adam.setDescription("Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods-and imprisoned just as quickly-Black Adam (Johnson) is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.");
+        Movie the_enforcer = new Movie("The Enforcer");
+        the_enforcer.setDescription("An enforcer has to sacrifice everything to save a young girl he has befriended from his femme fatale boss who is involved in cybersex trafficking.");
+        movieService.add(black_adam);
+        System.out.println(movieService.get(black_adam.getId()));
+        movieService.add(the_enforcer);
         movieService.getAll().forEach(System.out::println);
 
-        CinemaHall hall3D = new CinemaHall();
-        hall3D.setCapacity(50);
-        hall3D.setDescription("3D hall");
-        CinemaHall vipHall = new CinemaHall();
-        vipHall.setCapacity(10);
-        vipHall.setDescription("VIP hall");
-        cinemaHallService.add(hall3D);
-        cinemaHallService.add(vipHall);
+        CinemaHall hallImax = new CinemaHall();
+        hallImax.setCapacity(100);
+        hallImax.setDescription("IMAX hall");
+        CinemaHall luxHall = new CinemaHall();
+        luxHall.setCapacity(10);
+        luxHall.setDescription("LUX hall");
+        cinemaHallService.add(hallImax);
+        cinemaHallService.add(luxHall);
         cinemaHallService.getAll().forEach(System.out::println);
 
         MovieSession morningSession = new MovieSession();
-        morningSession.setCinemaHall(hall3D);
-        morningSession.setMovie(harryPotter);
-        morningSession.setShowTime(LocalDateTime.of(2022, 9,
-                30, 8, 30));
+        morningSession.setCinemaHall(hallImax);
+        morningSession.setMovie(the_enforcer);
+        morningSession.setShowTime(LocalDateTime.of(2022, 10,
+                3, 8, 30));
         MovieSession daySession = new MovieSession();
-        daySession.setCinemaHall(hall3D);
-        daySession.setMovie(fastAndFurious);
-        daySession.setShowTime(LocalDateTime.of(2022, 9,
-                30, 13, 00));
+        daySession.setCinemaHall(hallImax);
+        daySession.setMovie(black_adam);
+        daySession.setShowTime(LocalDateTime.of(2022, 10,
+                3, 13, 00));
         MovieSession eveningSession = new MovieSession();
-        eveningSession.setCinemaHall(vipHall);
-        eveningSession.setMovie(harryPotter);
-        eveningSession.setShowTime(LocalDateTime.of(2022, 9,
-                30, 17, 20));
+        eveningSession.setCinemaHall(luxHall);
+        eveningSession.setMovie(the_enforcer);
+        eveningSession.setShowTime(LocalDateTime.of(2022, 10,
+                3, 17, 20));
         MovieSession nightSession = new MovieSession();
-        nightSession.setCinemaHall(vipHall);
-        nightSession.setMovie(fastAndFurious);
-        nightSession.setShowTime(LocalDateTime.of(2022, 9,
-                30, 22, 40));
+        nightSession.setCinemaHall(luxHall);
+        nightSession.setMovie(black_adam);
+        nightSession.setShowTime(LocalDateTime.of(2022, 10,
+                3, 22, 40));
         movieSessionService.add(morningSession);
         movieSessionService.add(daySession);
         movieSessionService.add(eveningSession);
         movieSessionService.add(nightSession);
 
         System.out.println(movieSessionService.get(daySession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(harryPotter.getId(),
-                LocalDate.of(2022, 9, 30)));
+        System.out.println(movieSessionService.findAvailableSessions(the_enforcer.getId(),
+                LocalDate.of(2022, 10, 3)));
     }
 }
