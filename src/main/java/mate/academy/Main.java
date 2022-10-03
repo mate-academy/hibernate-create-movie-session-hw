fe29b82784c5bcfecdae66d88b1b9159c2863ec6
fliarkovskyi <fliarkovskyi@gmail.com>
2022-10-03 15:32:39 +0300

    fix name movie

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -21,13 +21,18 @@ public class Main {
             (CinemaHallService) injector.getInstance(CinemaHallService.class);
 
     public static void main(String[] args) {
-        Movie black_adam = new Movie("Black Adam");
-        black_adam.setDescription("Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods-and imprisoned just as quickly-Black Adam (Johnson) is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.");
-        Movie the_enforcer = new Movie("The Enforcer");
-        the_enforcer.setDescription("An enforcer has to sacrifice everything to save a young girl he has befriended from his femme fatale boss who is involved in cybersex trafficking.");
-        movieService.add(black_adam);
-        System.out.println(movieService.get(black_adam.getId()));
-        movieService.add(the_enforcer);
+        Movie blackAdam = new Movie("Black Adam");
+        blackAdam.setDescription("Nearly 5,000 years after was bestowed with the almighty powers "
+                + "of the Egyptian gods-and imprisoned just as quickly-Black"
+                + " Adam (Johnson) is freed from his earthly tomb, "
+                + "ready to unleash his unique form of justice on the modern world.");
+        Movie theEnforcer = new Movie("The Enforcer");
+        theEnforcer.setDescription("An enforcer has to sacrifice everything to save a young girl "
+                + "he has befriended from his femme fatale "
+                + "boss who is involved in cybersex trafficking.");
+        movieService.add(blackAdam);
+        System.out.println(movieService.get(blackAdam.getId()));
+        movieService.add(theEnforcer);
         movieService.getAll().forEach(System.out::println);
 
         CinemaHall hallImax = new CinemaHall();
@@ -42,22 +47,22 @@ public class Main {
 
         MovieSession morningSession = new MovieSession();
         morningSession.setCinemaHall(hallImax);
-        morningSession.setMovie(the_enforcer);
+        morningSession.setMovie(theEnforcer);
         morningSession.setShowTime(LocalDateTime.of(2022, 10,
                 3, 8, 30));
         MovieSession daySession = new MovieSession();
         daySession.setCinemaHall(hallImax);
-        daySession.setMovie(black_adam);
+        daySession.setMovie(blackAdam);
         daySession.setShowTime(LocalDateTime.of(2022, 10,
                 3, 13, 00));
         MovieSession eveningSession = new MovieSession();
         eveningSession.setCinemaHall(luxHall);
-        eveningSession.setMovie(the_enforcer);
+        eveningSession.setMovie(theEnforcer);
         eveningSession.setShowTime(LocalDateTime.of(2022, 10,
                 3, 17, 20));
         MovieSession nightSession = new MovieSession();
         nightSession.setCinemaHall(luxHall);
-        nightSession.setMovie(black_adam);
+        nightSession.setMovie(blackAdam);
         nightSession.setShowTime(LocalDateTime.of(2022, 10,
                 3, 22, 40));
         movieSessionService.add(morningSession);
@@ -66,7 +71,7 @@ public class Main {
         movieSessionService.add(nightSession);
 
         System.out.println(movieSessionService.get(daySession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(the_enforcer.getId(),
+        System.out.println(movieSessionService.findAvailableSessions(theEnforcer.getId(),
                 LocalDate.of(2022, 10, 3)));
     }
 }
