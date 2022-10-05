5cc1ce93f47d2a0175313fa093b0159d5c8e2e0f
Serhii Buria <sergeiburya@gmail.com>
2022-10-05 10:55:09 +0300

    added CinemaHall and MovieSession classes

--- src/main/java/mate/academy/model/CinemaHall.java
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -1,4 +1,69 @@
 package mate.academy.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cinema_halls")
 public class CinemaHall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int capacity;
+    private String description;
+
+    public CinemaHall() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id,capacity, description);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        CinemaHall cinemaHall = (CinemaHall) obj;
+        return Objects.equals(id, cinemaHall.id)
+                && Objects.equals(description, cinemaHall.description)
+                && capacity == cinemaHall.capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "CinemaHall{"
+                + "id=" + id
+                + ", capacity=" + capacity
+                + ", description='" + description + '\''
+                + '}';
+    }
 }
--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -1,4 +1,86 @@
 package mate.academy.model;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "movie_sessions")
 public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+    @ManyToOne
+    @JoinColumn(name = "cinema_hall_id")
+    private CinemaHall cinemaHall;
+    @Column(name = "show_time")
+    private LocalDateTime showTime;
+
+    public MovieSession() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public CinemaHall getCinemaHall() {
+        return cinemaHall;
+    }
+
+    public void setCinemaHall(CinemaHall cinemaHall) {
+        this.cinemaHall = cinemaHall;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime localDateTime) {
+        this.showTime = localDateTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, movie, cinemaHall, showTime);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        MovieSession movieSession = (MovieSession) obj;
+        return Objects.equals(id, movieSession.id) &&
+                Objects.equals(movie, movieSession.movie) &&
+                Objects.equals(cinemaHall, movieSession.cinemaHall) &&
+                Objects.equals(showTime, movieSession.showTime);
+    }
+
+    @Override
+    public String toString() {
+        return "MovieSession{"
+                + "id=" + id
+                + ", movie=" + movie
+                + ", cinemaHall=" + cinemaHall
+                + ", localDateTime=" + showTime
+                + '}';
+    }
 }
