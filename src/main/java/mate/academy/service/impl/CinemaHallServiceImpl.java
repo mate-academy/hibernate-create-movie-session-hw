78a19cabaab59329302d43c9bf955c0785235eb4
AndrewLinskii <tolstuy1996@gmail.com>
2022-10-18 17:46:15 +0300

    add massage

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -19,7 +19,8 @@ public class CinemaHallServiceImpl implements CinemaHallService {
 
     @Override
     public CinemaHall get(Long id) {
-        return cinemaHallDao.get(id).orElseThrow();
+        return cinemaHallDao.get(id).orElseThrow(
+                () -> new RuntimeException("CinemaHall whit id: " + id + " not found"));
     }
 
     @Override
