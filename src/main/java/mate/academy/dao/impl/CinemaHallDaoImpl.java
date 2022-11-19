23c2b0a5abf365ab77d8f07560a1398b5412ce7d
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-11-19 13:37:27 +0200

    Create CinemaHallDaoImpl and CinemaHallServiceImpl

---
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,60 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.CinemaHall;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(cinemaHall);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save cinema hall", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return cinemaHall;
+    }
+
+    @Override
+    public Optional<CinemaHall> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(CinemaHall.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get cinema hall by id " + id, e);
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        List<CinemaHall> resultList;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String query = "from CinemaHall";
+            Query<CinemaHall> getAllCinemaHalls = session.createQuery(query, CinemaHall.class);
+            resultList = getAllCinemaHalls.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get list of all cinema halls", e);
+        }
+        return resultList;
+    }
+}
--- src/main/java/mate/academy/service/MovieService.java
+++ src/main/java/mate/academy/service/MovieService.java
@@ -3,6 +3,7 @@ package mate.academy.service;
 import java.util.List;
 import mate.academy.model.Movie;
 
+
 public interface MovieService {
     Movie add(Movie movie);
 
