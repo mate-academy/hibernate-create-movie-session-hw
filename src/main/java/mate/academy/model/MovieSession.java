f60f4c6767d44911b8ebea463379f0c0838babf0
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-11-19 13:12:07 +0200

    Create MovieSession class

---
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -0,0 +1,72 @@
+package mate.academy.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movie_sessions")
+public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Movie movie;
+    @ManyToOne
+    private CinemaHall cinemaHall;
+    private LocalDateTime showtime;
+
+    public MovieSession() {
+    }
+
+    public MovieSession(LocalDateTime showtime) {
+        this.showtime = showtime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public CinemaHall getCinemaHall() {
+        return cinemaHall;
+    }
+
+    public void setCinemaHall(CinemaHall cinemaHall) {
+        this.cinemaHall = cinemaHall;
+    }
+
+    public LocalDateTime getShowtime() {
+        return showtime;
+    }
+
+    public void setShowtime(LocalDateTime showtime) {
+        this.showtime = showtime;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieSession{" +
+                "id=" + id +
+                ", movie=" + movie +
+                ", cinemaHall=" + cinemaHall +
+                ", showtime=" + showtime +
+                '}';
+    }
+}
