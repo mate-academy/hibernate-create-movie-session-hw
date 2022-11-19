b2097715c8351854f28dee3f32141a9c2c0edb06
shmax <mshveda28@gmail.com>
2022-11-19 15:13:09 +0200

    Configure hibernate; add MovieSession and CinemaHall models

--- src/main/java/mate/academy/model/CinemaHall.java
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -14,12 +13,12 @@ import lombok.RequiredArgsConstructor;
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
-@Table(name = "movies")
-public class Movie {
+public class CinemaHall {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NonNull
-    private String title;
+    private int capacity;
+    @NonNull
     private String description;
 }
--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -14,12 +14,14 @@ import lombok.RequiredArgsConstructor;
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
-@Table(name = "movies")
-public class Movie {
+public class MovieSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
     @NonNull
-    private String title;
-    private String description;
+    private Movie movie;
+    @ManyToOne
+    @NonNull
+    private CinemaHall cinemaHall;
 }
