6d835773a5c1ffeedd43be37a64e527144abe484
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-11-28 17:37:59 +0200

    added fixes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,8 +1,15 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
@@ -15,5 +22,23 @@ public class Main {
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall cinemaHall = new CinemaHall(50, "Cinema Star Hall");
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        cinemaHallService.add(cinemaHall);
+        System.out.println(cinemaHallService.get(cinemaHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        LocalDateTime showTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 0));
+        MovieSession movieSession = new MovieSession(fastAndFurious, cinemaHall, showTime);
+
+        MovieSessionService movieSessionService =
+                (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+        System.out.println(movieSessionService.get(movieSession.getId()));
+        movieSessionService
+                .findAvailableSessions(movieSession.getId(), LocalDate.now())
+                .forEach(System.out::println);
     }
 }
