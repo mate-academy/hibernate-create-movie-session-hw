6b4681a52a0b97756c73eb2cff20a6a98177844b
GrishaSeniv <seniv.grisha@gmail.com>
2022-12-02 00:44:57 +0200

    implemented CinemaHallDao, MovieDao, MovieSessionDao, added hibernate configuration

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,45 @@
 package mate.academy;
 
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final LocalDateTime showTime = LocalDateTime.of(2022, 11, 25, 15, 0);
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        CinemaHall kingCross = new CinemaHall();
+        kingCross.setDescription("A large hall with various effects");
+        kingCross.setCapacity(100);
+        cinemaHallService.add(kingCross);
+        System.out.println(cinemaHallService.get(kingCross.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(fastAndFurious);
+        movieSession.setCinemaHall(kingCross);
+        movieSession.setShowTime(showTime);
+        MovieSessionService movieSessionService =
+                (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+        System.out.println(movieSessionService.get(movieSession.getId()));
+        movieSessionService.findAvailableSessions(fastAndFurious.getId(), showTime.toLocalDate())
+                .forEach(System.out::println);
     }
 }
