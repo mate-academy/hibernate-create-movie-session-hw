84b14bd87739aaccdfd6fba001ac5d3ede4503c3
zipic <zpc23041988@gmail.com>
2022-12-04 14:21:36 +0200

    movie_session complete

---
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,48 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.CinemaHall;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(cinemaHall);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't create cinemaHall " + cinemaHall, e);
+        }
+        return cinemaHall;
+    }
+
+    @Override
+    public Optional<CinemaHall> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CinemaHall cinemaHall = session.get(CinemaHall.class, id);
+            return Optional.ofNullable(cinemaHall);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get cinema hall by id " + id, e);
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        return null;
+    }
+}
