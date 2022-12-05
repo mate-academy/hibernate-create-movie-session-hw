ecd8583d2f468812c47fcaa87c999b6d2988e4ea
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-12-05 14:02:49 +0200

    Some fixes after student review

--- src/main/java/mate/academy/service/CinemaHallService.java
+++ src/main/java/mate/academy/service/CinemaHallService.java
@@ -3,7 +3,8 @@ package mate.academy.service;
 import java.util.List;
 import mate.academy.model.CinemaHall;
 
-public interface CinemaHallService { CinemaHall add(CinemaHall cinemaHall);
+public interface CinemaHallService {
+    CinemaHall add(CinemaHall cinemaHall);
 
     CinemaHall get(Long id);
 
