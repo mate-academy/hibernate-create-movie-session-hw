662567d2bde26d7bbd026efa64deb97bc6545898
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-12-05 23:02:37 +0200

    added optional in some cases

--- src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -3,6 +3,8 @@ package mate.academy.dao.impl;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+
 import mate.academy.dao.MovieSessionDao;
 import mate.academy.lib.Dao;
 import mate.academy.model.MovieSession;
@@ -40,9 +42,9 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
     }
 
     @Override
-    public MovieSession get(Long id) {
+    public Optional<MovieSession> get(Long id) {
         try (Session session = sessionFactory.openSession()) {
-            return session.get(MovieSession.class, id);
+            return Optional.ofNullable(session.get(MovieSession.class, id));
         }
     }
 
