0a023c4b1ef4db99a1e6a3339e15e7ca9231b2c7
Iryna Chernikova <i.chernikova14@ukr.net>
2022-12-07 11:47:51 +0200

    moved injector to class variables, added annotations in MovieSession

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -1,10 +1,12 @@
 package mate.academy.model;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -17,7 +19,9 @@ public class MovieSession {
     @ManyToOne
     private Movie movie;
     @ManyToOne
+    @JoinColumn(name = "cinema_hall_id")
     private CinemaHall cinemaHall;
+    @Column(name = "show_time")
     private LocalDateTime showTime;
 
     public MovieSession() {
