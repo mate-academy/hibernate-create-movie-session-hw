e6b5bbaee146937ffc174bef2f8eb86d0ba2ddf1
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-07 20:50:27 +0200

    add check in main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -7,7 +7,8 @@ import mate.academy.model.MovieSession;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
-import mate.academy.service.impl.MovieServiceImpl;
+
+import java.time.LocalDateTime;
 
 public class Main {
     private static Injector injector = Injector.getInstance("mate");
@@ -24,5 +25,14 @@ public class Main {
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall superHall = new CinemaHall(240 , "Cool hall");
+        cinemaHallService.add(superHall);
+        System.out.println(movieService.get(superHall.getId()));
+
+        MovieSession fastAndFuriousSession = new MovieSession(LocalDateTime.now(), superHall, fastAndFurious);
+        movieSessionService.add(fastAndFuriousSession);
+        System.out.println(movieSessionService
+                .findAvailableSessions(fastAndFurious.getId(), LocalDateTime.now()));
     }
 }
