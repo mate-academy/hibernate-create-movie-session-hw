36d4e24fa6131fc3ec159e1b2b1700c0c9f37d85
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2023-01-17 08:25:02 +0200

    added models

---
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -0,0 +1,60 @@
+package mate.academy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cinema_halls")
+public class CinemaHall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int capacity;
+    private String description;
+
+    public CinemaHall() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.append("CinemaHall{")
+                .append("id=")
+                .append(id)
+                .append(", capacity=")
+                .append(capacity)
+                .append(", description='")
+                .append(", description='")
+                .append(description)
+                .append('\'')
+                .append('}')
+                .toString();
+    }
+}
