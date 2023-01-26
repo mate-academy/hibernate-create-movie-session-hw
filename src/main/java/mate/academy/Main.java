339b0a82cd35ee5eb13e3835f7389125d47c154e
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2023-01-26 12:27:51 +0200

    Added hibernate.cfg.xml file, created CinemaHall, MovieSession classes, added relations between classes. Also created CinemaHallService and CinemaHallDao interfaces and implementation to them. Besides, created MovieSessionService and MovieSessionDao interfaces and implementation.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,27 @@
 package mate.academy;
 
-import mate.academy.model.Movie;
+import java.time.LocalDate;
+import mate.academy.lib.Injector;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
+    public static void main(String[] args) {
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        MovieSessionService movieSessionService =
+                (MovieSessionService) injector.getInstance(MovieSessionService.class);
+
+        MovieSession movieSession = movieSessionService.get(5L);
+        System.out.println(movieSession);
+
+        System.out.println(movieSessionService.findAvailableSessions(4L,
+                LocalDate.of(2023, 1, 25)));
     }
 }
