a5277d6ac831778c406793a85f91e6c92cf03450
Mykola Radzivon <kolian201497@gmail.com>
2023-02-15 09:07:45 +0200

    removed empty line, and added foreach for receive data from available session

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -13,26 +13,21 @@ import mate.academy.service.MovieSessionService;
 public class Main {
     public static void main(String[] args) {
         Injector injector = Injector.getInstance("mate.academy");
-
         MovieService movieService =
                 (MovieService) injector.getInstance(MovieService.class);
-
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
-
         CinemaHallService cinemaHallService =
                 (CinemaHallService) injector.getInstance(CinemaHallService.class);
-
         CinemaHall cinemaHall = new CinemaHall();
         cinemaHall.setCapacity(200);
         cinemaHall.setDescription("CinemaTheatre in Ocean Plaza");
         cinemaHallService.add(cinemaHall);
         System.out.println(movieService.get(cinemaHall.getId()));
         movieService.getAll().forEach(System.out::println);
-
         LocalDateTime datetime = LocalDateTime.of(2022, 1, 2, 10, 15);
         MovieSession movieSession = new MovieSession();
         movieSession.setMovie(fastAndFurious);
@@ -40,12 +35,11 @@ public class Main {
         movieSession.setShowTime(datetime);
         MovieSessionService movieSessionService =
                 (MovieSessionService) injector.getInstance(MovieSessionService.class);
-
         movieSessionService.add(movieSession);
         System.out.println(movieSessionService.get(movieSession.getId()));
         List<MovieSession> availableSessions = movieSessionService
                 .findAvailableSessions(fastAndFurious.getId(), datetime.toLocalDate());
-        System.out.println(availableSessions.get(0).getCinemaHall().getDescription());
+        availableSessions.forEach(a -> System.out.println(a.getCinemaHall().getDescription()));
         MovieSession movieSession1 = movieSessionService.get(movieSession.getId());
     }
 }
