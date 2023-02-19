ad3062eeb0ed6177995a3b8fb01e49b75ca071ca
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-02-19 17:13:30 +0200

    Added implementations of getAll() method to MovieDaoImpl and MovieServiceImpl, created Injector in Main class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,9 +1,12 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
         MovieService movieService = null;
 
