a22bae51058b6fb9908c0cc42cc69a650154a7b5
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2023-02-19 16:03:15 +0200

    Created MovieSession class

--- src/main/java/mate/academy/model/CinemaHall.java
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -31,10 +31,10 @@ public class CinemaHall {
 
     @Override
     public String toString() {
-        return "CinemaHall{" +
-                "id=" + id +
-                ", capacity=" + capacity +
-                ", description='" + description + '\'' +
-                '}';
+        return "CinemaHall{"
+                + "id=" + id
+                + ", capacity=" + capacity
+                + ", description='" + description + '\''
+                + '}';
     }
 }
