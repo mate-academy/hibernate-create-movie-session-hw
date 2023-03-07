4d01ae96771488564f0c75f302be4108628f82fe
Denis Pugovkin <den.pugovkin@gmail.com>
2023-03-07 19:53:41 +0200

    Commit 1. All models were created. 2. Method on DAO level and Service level were added and implemented. 3. File for hibernate configuration was added and configured (src/main/resources/hibernate.cfg.xml). 4. Annotations for injector and hibernate correctly working were added. 5. All methods were tested successfully.

---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,63 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Couldn't add movie session "
+                    + movieSession + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<MovieSession> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(MovieSession.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Couldn't add movie session by id: "
+                    + id + " to DB", e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> getAllMovieSessionInCurrentDay =
+                    session.createQuery("FROM MovieSession m WHERE m.showtime >= :value1 "
+                                    + "AND m.showtime <= :value2 AND m.movie.id = :value3",
+                            MovieSession.class);
+            getAllMovieSessionInCurrentDay.setParameter("value1", date.atTime(00, 00));
+            getAllMovieSessionInCurrentDay.setParameter("value2", date.atTime(23,59));
+            getAllMovieSessionInCurrentDay.setParameter("value3", movieId);
+            return getAllMovieSessionInCurrentDay.getResultList();
+        }
+    }
+}
