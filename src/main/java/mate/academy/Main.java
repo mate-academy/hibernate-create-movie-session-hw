d0056ded1c0c2acb5c135c741d9de724e44861e3
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2023-03-12 16:18:58 +0200

    hw-movie-session solution -added configuration file hibernate.cfg.xml; -created CinemaHall class with its DAO and Service layers; -created MovieSession class with its DAO and Service layers; -used annotations from lib package for dependency injection.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,47 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
+    public static void main(String[] args) {
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
+
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall emeraldCinemaHall = new CinemaHall();
+        emeraldCinemaHall.setCapacity(247);
+        emeraldCinemaHall.setDescription("With love seats");
+
+        CinemaHallService cinemaHallService
+                = (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        cinemaHallService.add(emeraldCinemaHall);
+        System.out.println(cinemaHallService.get(emeraldCinemaHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSession fastAndFuriousTodaySession = new MovieSession();
+        fastAndFuriousTodaySession.setCinemaHall(emeraldCinemaHall);
+        fastAndFuriousTodaySession.setMovie(fastAndFurious);
+        fastAndFuriousTodaySession.setShowTime(LocalDateTime.now());
+
+        MovieSessionService movieSessionService
+                = (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(fastAndFuriousTodaySession);
+        System.out.println(movieSessionService.get(fastAndFuriousTodaySession.getId()));
+        movieSessionService.findAvailableSessions(fastAndFurious.getId(), LocalDate.now())
+                .forEach(System.out::println);
     }
 }
--- src/main/java/mate/academy/model/CinemaHall.java
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -4,20 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class Movie {
+@Table(name = "cinema_halls")
+public class CinemaHall {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
+    private int capacity;
     private String description;
 
-    public Movie() {
-    }
-
-    public Movie(String title) {
-        this.title = title;
+    public CinemaHall() {
     }
 
     public Long getId() {
@@ -28,12 +26,12 @@ public class Movie {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public int getCapacity() {
+        return capacity;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 
     public String getDescription() {
@@ -46,9 +44,9 @@ public class Movie {
 
     @Override
     public String toString() {
-        return "Movie{"
+        return "CinemaHall{"
                 + "id=" + id
-                + ", title='" + title + '\''
+                + ", capacity=" + capacity
                 + ", description='" + description + '\''
                 + '}';
     }
