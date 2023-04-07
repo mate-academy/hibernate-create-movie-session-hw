822faf3090ee47c140e3a1638a55e37712f50502
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-07 12:54:20 +0300

    implement getAll() in MovieDaoImpl and MovieServiceImpl

--- src/main/java/mate/academy/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -19,11 +19,12 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie get(Long id) {
-        return movieDao.get(id).get();
+        return movieDao.get(id).orElseThrow(() ->
+                new RuntimeException("Movie does not exist by id:" + id));
     }
 
     @Override
     public List<Movie> getAll() {
-        return null;
+        return movieDao.getAll();
     }
 }
