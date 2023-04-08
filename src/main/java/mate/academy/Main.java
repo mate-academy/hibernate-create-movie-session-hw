55e44ab877190c15674a8f21c49a11b9c2081ec9
Serhii Shyian <serhii.shiyan@ukr.net>
2023-04-08 11:53:12 +0300

    1) Updated dependencies in pom.xml 2) Created hibernate.cfg.xml in resources package 3) Created init_db.sql in resources package 4) Created CinemaHall and MovieSession classes in model package 5) Implemented getAll() method in MovieDaoImpl and MovieServiceImpl classes 6) Created CinemaHallDaoImpl and CinemaHallServiceImpl and implemented there methods add(), get(), getAll() 7) In Main class added tests for all methods in MovieServiceImpl, CinemaHallServiceImpl and MovieSessionDaoImpl classes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,130 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
+    private static final Injector injector
+            = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        MovieService movieService = null;
+        MovieService movieService
+                = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
+
+        Movie tetris = new Movie("Tetris");
+        tetris.setDescription("A game that you can't stop playing. "
+                + "A story that can't be imagined");
+
+        System.out.println("----------------------------");
+        System.out.println("Start testing MovieService.");
+
+        System.out.println("----------------------------");
+        System.out.println("Add method was called.");
+        System.out.println(movieService.add(tetris));
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("Get by id method was called.");
+        System.out.println(movieService.get(tetris.getId()));
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("GetAll method was called.");
         movieService.getAll().forEach(System.out::println);
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("All tests finished.");
+
+        CinemaHallService cinemaHallService
+                = (CinemaHallService) injector.getInstance(CinemaHallService.class);
+
+        CinemaHall blueHall = new CinemaHall(
+                "4DX hall is action and spectacle "
+                + "prescribes special effects for every blockbuster or cartoon "
+                + "sometimes together with the director "
+                + "The effects track is thought out and synchronized to the second and "
+                + "controlled by a computer");
+        blueHall.setCapacity(50);
+        cinemaHallService.add(blueHall);
+
+        CinemaHall yellowHall = new CinemaHall(
+                "IMAX hall is a proprietary system"
+                + " of high-resolution cameras, film formats, film projectors, "
+                + "and theaters known for having very large screens "
+                + "and steep stadium seating.");
+        yellowHall.setCapacity(100);
+
+        System.out.println("----------------------------");
+        System.out.println("Start testing CinemaHallService.");
+
+        System.out.println("----------------------------");
+        System.out.println("Add method was called.");
+        System.out.println(cinemaHallService.add(yellowHall));
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("Get by id method was called.");
+        System.out.println(cinemaHallService.get(yellowHall.getId()));
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("GetAll method was called.");
+        cinemaHallService.getAll().forEach(System.out::println);
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("All tests finished.");
+
+        MovieSessionService movieSessionService
+                = (MovieSessionService) injector.getInstance(MovieSessionService.class);
+
+        MovieSession fastAndFuriousMovieSession = new MovieSession(
+                movieService.get(fastAndFurious.getId()),
+                cinemaHallService.get(blueHall.getId()),
+                LocalDateTime.now().plusDays(2).plusHours(4)
+        );
+        movieSessionService.add(fastAndFuriousMovieSession);
+
+        MovieSession tetrisMovieSession = new MovieSession(
+                movieService.get(tetris.getId()),
+                cinemaHallService.get(yellowHall.getId()),
+                LocalDateTime.now().plusDays(4).minusHours(8)
+        );
+
+        System.out.println("----------------------------");
+        System.out.println("Start testing MovieSessionService.");
+
+        System.out.println("----------------------------");
+        System.out.println("Add method was called.");
+        System.out.println(movieSessionService.add(tetrisMovieSession));
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("Get by id method was called.");
+        System.out.println(movieSessionService.get(tetrisMovieSession.getId()));
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("FindAvailableSessions method was called.");
+        System.out.println(movieSessionService.findAvailableSessions(
+                tetris.getId(),
+                LocalDate.from(LocalDateTime.now().plusDays(4).minusHours(8))
+                ));
+        System.out.println("Done.");
+
+        System.out.println("----------------------------");
+        System.out.println("All tests finished.");
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,68 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movieSession " + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<MovieSession> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(MovieSession.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a movieSession by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        LocalDateTime beginOfDay = LocalTime.MIN.atDate(date);
+        LocalDateTime endOfDay = LocalTime.MAX.atDate(date);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> findAvailableSessions = session.createQuery(
+                    "FROM MovieSession "
+                            + "WHERE movie.id = :movieIdSession "
+                            + "AND showTime BETWEEN :beginOfDay AND :endOfDay",
+                    MovieSession.class);
+            findAvailableSessions.setParameter("movieIdSession", movieId);
+            findAvailableSessions.setParameter("beginOfDay", beginOfDay);
+            findAvailableSessions.setParameter("endOfDay", endOfDay);
+            return findAvailableSessions.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find available sessions", e);
+        }
+    }
+}
