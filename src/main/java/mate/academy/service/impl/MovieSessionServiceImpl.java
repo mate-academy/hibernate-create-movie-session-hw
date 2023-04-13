b59a3ee2813e6f90750f359d64f7ff9b8ddc8c01
o-shyshkan <o.shyshkan@gmail.com>
2023-04-13 18:17:21 +0300

    fix errors 2

--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -20,7 +20,8 @@ public class MovieSessionServiceImpl implements MovieSessionService {
 
     @Override
     public MovieSession get(Long id) {
-        return movieSessionDao.get(id).get();
+        return movieSessionDao.get(id).orElseThrow(() -> new RuntimeException("No value"
+                + " found for MovieSession id:" + id));
     }
 
     @Override
