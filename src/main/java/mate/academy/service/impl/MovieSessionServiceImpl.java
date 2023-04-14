6567b1538f0e7aa0d010f8f4c499af7a1db2651a
Artem Grunin <artem.grunin.2006@gmail.com>
2023-04-15 01:05:38 +0300

    fixed messege in Movie Session Service

--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -22,7 +22,7 @@ public class MovieSessionServiceImpl implements MovieSessionService {
     @Override
     public MovieSession get(Long id) {
         return movieSessionDao.get(id).orElseThrow(() ->
-                new EntityNotFoundException("No one movie session with" + id + "was found"));
+                new EntityNotFoundException("Can`t get movie session by id " + id));
     }
 
     @Override
