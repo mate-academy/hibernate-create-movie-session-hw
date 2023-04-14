0df9a2a8731a2b9e14717850c2ae7af081689c7b
Artem Grunin <artem.grunin.2006@gmail.com>
2023-04-15 01:39:12 +0300

    updated hibernate version

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,11 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
@@ -11,10 +15,10 @@ public class Main {
             Injector.getInstance("mate.academy");
     private static final MovieService movieService =
             (MovieService) injector.getInstance(MovieService.class);
-    private static final CinemaHallService cinemaHallService = (CinemaHallService) injector
-            .getInstance(CinemaHallService.class);
-    private static final MovieSessionService movieSessionService = (MovieSessionService) injector
-            .getInstance(MovieSessionService.class);
+    private static final CinemaHallService cinemaHallService =
+            (CinemaHallService) injector.getInstance(CinemaHallService.class);
+    private static final MovieSessionService movieSessionService =
+            (MovieSessionService) injector.getInstance(MovieSessionService.class);
 
     public static void main(String[] args) {
         Movie fastAndFurious = new Movie("Fast and Furious");
@@ -23,6 +27,23 @@ public class Main {
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
 
+        CinemaHall cronverkCinemaVipHall = new CinemaHall();
+        cronverkCinemaVipHall.setCapacity(8);
+        cronverkCinemaVipHall.setDescription("VIP cinema hall");
+        cinemaHallService.add(cronverkCinemaVipHall);
+        System.out.println(cinemaHallService.get(cronverkCinemaVipHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
 
+        LocalDate dateForCinema = LocalDate.of(2022, 2, 24);
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(fastAndFurious);
+        movieSession.setCinemaHall(cronverkCinemaVipHall);
+        LocalDateTime showTime = dateForCinema.atTime(4,30);
+        movieSession.setShowTime(showTime);
+
+        movieSessionService.add(movieSession);
+        System.out.println(movieSessionService.get(movieSession.getId()));
+        movieSessionService.findAvailableSessions(fastAndFurious.getId(), dateForCinema)
+                .forEach(System.out::println);
     }
 }
