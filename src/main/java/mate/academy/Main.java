ed9a991953e8a4944a40e4d997a0031051d5ea95
Oleksandr Krasnov <redman2775@gmail.com>
2023-04-18 20:33:45 +0300

    fix order

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -11,7 +11,7 @@ import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
 
 public class Main {
-    private final static int HALL_CAPACITY = 20;
+    private static final int HALL_CAPACITY = 20;
     private static final Injector injector = Injector.getInstance("mate.academy");
     private static final MovieService movieService =
             (MovieService) injector.getInstance(MovieService.class);
