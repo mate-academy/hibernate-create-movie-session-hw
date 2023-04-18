6a788325c83c2303e0afc7707d28ee1220051581
KosMarch <kosmarchenko98@gmail.com>
2023-04-18 08:57:34 +0300

    changed exception

--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -2,6 +2,7 @@ package mate.academy.service.impl;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.academy.dao.MovieSessionDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
@@ -21,7 +22,7 @@ public class MovieSessionServiceImpl implements MovieSessionService {
     @Override
     public MovieSession get(Long id) {
         return movieSessionDao.get(id).orElseThrow(() ->
-                new RuntimeException("Can't get movie session by id" + id));
+                new NoSuchElementException("Can't get movie session by id" + id));
     }
 
     @Override
