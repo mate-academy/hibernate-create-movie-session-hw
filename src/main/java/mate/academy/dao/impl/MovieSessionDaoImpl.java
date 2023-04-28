b0cef818a283a78e45b169d90cfc3e9903fde688
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-04-28 13:16:58 +0300

    Added Optional

--- src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -3,6 +3,7 @@ package mate.academy.dao.impl;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import mate.academy.dao.MovieSessionDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
@@ -38,11 +39,11 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
     }
 
     @Override
-    public MovieSession get(Long id) {
+    public Optional<MovieSession> get(Long id) {
         Session session = null;
         try {
             session = HibernateUtil.getSessionFactory().openSession();
-            return session.get(MovieSession.class, id);
+            return Optional.ofNullable(session.get(MovieSession.class, id));
         } catch (Exception e) {
             throw new DataProcessingException("Can't get movie session by id: " + id, e);
         } finally {
