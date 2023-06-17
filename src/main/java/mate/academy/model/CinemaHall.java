8d6ab02203be2d7fccbe6c66263a9c2aee08e3ee
Dmytro Katrevych <katrevichdv@gmail.com>
2023-06-17 19:06:39 +0300

    hibernate.cfg.xml was created. CinemaHall and MovieSession were created. MovieDaoImpl and MovieServiceImpl getAll() methods implementations were added. CinemaHallDaoImpl and CinemaHallServiceImpl were created. MovieSessionDaoImpl and MovieSessionServiceImpl were created. main() method was changed. minor changes were added.

---
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -0,0 +1,48 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class CinemaHall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer capacity;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CinemaHall{"
+                + "id=" + id
+                + ", capacity=" + capacity
+                + ", name='" + name + '\''
+                + '}';
+    }
+}
