713437169150278f8213b50ef18143495bba8f3e
Victor Ruban <edmont373@gmail.com>
2023-07-06 20:35:35 +0300

    create all

---
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -0,0 +1,23 @@
+package mate.academy.service.impl;
+
+import mate.academy.model.CinemaHall;
+import mate.academy.service.CinemaHallService;
+
+import java.util.List;
+
+public class CinemaHallServiceImpl implements CinemaHallService {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        return null;
+    }
+
+    @Override
+    public CinemaHall get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        return null;
+    }
+}
