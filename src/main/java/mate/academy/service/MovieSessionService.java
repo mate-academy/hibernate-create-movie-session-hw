23b754ea47cfb08b44132a20503e60af284b5453
valdemar <vova.malyarchuk2@gmail.com>
2023-07-07 14:50:19 +0300

    Implemented task sessions

---
+++ src/main/java/mate/academy/service/MovieSessionService.java
@@ -0,0 +1,13 @@
+package mate.academy.service;
+
+import java.util.List;
+import java.time.LocalDate;
+import mate.academy.model.MovieSession;
+
+public interface MovieSessionService {
+    MovieSession add(MovieSession movieSession);
+
+    MovieSession get(Long id);
+
+    List<MovieSession> findAvailableSessions(Long movieId, LocalDate date);
+}
