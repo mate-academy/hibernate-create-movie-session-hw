5e22c61fae5aff8deb4c7b5439aec9789490706c
heilxwotan1 <noobdima228@gmail.com>
2023-08-08 19:36:58 +0300

    hibernate-create-movie-session-hw solution 1. created dao interfaces and implemented it 2. created service interfaces and implemented it 3. added injector annotations to dao and service impl 4. creted CinemaHall and MovieSession models

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,56 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
+    public static void main(String[] args) {
+        final MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
+        final CinemaHallService cinemaHallService = (CinemaHallService) injector
+                .getInstance(CinemaHallService.class);
+        final MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
+
+        Movie oppenheimer = new Movie("Oppenheimer");
+        oppenheimer.setDescription("Now I'm become death");
+        Movie barbie = new Movie("Barbie");
+        barbie.setDescription("I'm KENOUGH");
+        movieService.add(oppenheimer);
+        movieService.add(barbie);
+
+        CinemaHall smallHall = new CinemaHall();
+        smallHall.setDescription("SMALL HALL");
+        smallHall.setCapacity(100);
+
+        CinemaHall bigHall = new CinemaHall();
+        bigHall.setDescription("BIG HALL");
+        bigHall.setCapacity(350);
+        cinemaHallService.add(smallHall);
+        cinemaHallService.add(bigHall);
+
+        MovieSession firstMovieSession = new MovieSession();
+        firstMovieSession.setMovie(movieService.get(1L).orElseThrow());
+        firstMovieSession.setCinemaHall(cinemaHallService.get(2L).orElseThrow());
+        firstMovieSession.setShowTime(LocalDateTime.of(2023, 8, 8, 12, 30));
+
+        MovieSession secondMovieSession = new MovieSession();
+        secondMovieSession.setMovie(movieService.get(2L).orElseThrow());
+        secondMovieSession.setCinemaHall(cinemaHallService.get(1L).orElseThrow());
+        secondMovieSession.setShowTime(LocalDateTime.of(2023, 9, 8, 14, 30));
+        movieSessionService.add(firstMovieSession);
+        movieSessionService.add(secondMovieSession);
+
+        System.out.println(movieSessionService.findAvailableSessions(2L, LocalDate.of(2023, 9, 8)));
+
     }
 }
