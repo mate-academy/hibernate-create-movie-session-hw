9d7ca989169c05db2d59b5f88e83ac95724a161b
Roman Kuchmii <romakuchmii@gmail.com>
2023-08-09 00:43:33 +0300

    added solution

---
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -0,0 +1,55 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.CinemaHall;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Session currentSession = null;
+        Transaction transaction = null;
+        try {
+            currentSession = HibernateUtil.getSessionFactory().openSession();
+            transaction = currentSession.beginTransaction();
+            currentSession.save(cinemaHall);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add cinema hall: " + cinemaHall,e);
+        } finally {
+            currentSession.close();
+        }
+        return cinemaHall;
+    }
+
+    @Override
+    public Optional<CinemaHall> get(Long id) {
+        try (Session currentSession = HibernateUtil.getSessionFactory().openSession();) {
+            return Optional.ofNullable(currentSession.get(CinemaHall.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        try (Session currentSession = HibernateUtil.getSessionFactory().openSession();) {
+            Query<CinemaHall> query =
+                    currentSession.createQuery("from CinemaHall ", CinemaHall.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all cinema halls", e);
+        }
+    }
+}
