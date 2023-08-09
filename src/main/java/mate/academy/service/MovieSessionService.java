d70c675714303f854fcce023b1911622125ee14c
Denis Moroz <moroz06denis@gmail.com>
2023-08-09 13:52:18 +0200

    improved some moments

--- src/main/java/mate/academy/service/MovieSessionService.java
+++ src/main/java/mate/academy/service/MovieSessionService.java
@@ -2,10 +2,8 @@ package mate.academy.service;
 
 import java.time.LocalDate;
 import java.util.List;
-import mate.academy.lib.Service;
 import mate.academy.model.MovieSession;
 
-@Service
 public interface MovieSessionService {
     MovieSession add(MovieSession movieSession);
 
