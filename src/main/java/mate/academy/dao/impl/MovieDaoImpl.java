780ca49f0c14fec8c3552dc53b2ae65732fe1182
Mykyta Zuber <nikitazuber@gmail.com>
2023-08-21 15:23:58 +0300

    Fixed code style, naming, added catchBlock

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -47,9 +47,9 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public List<Movie> getAll() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Movie> getAllCommentsQuery = session.createQuery(
+            Query<Movie> getAllMoviesQuery = session.createQuery(
                     "from Movie", Movie.class);
-            return getAllCommentsQuery.getResultList();
+            return getAllMoviesQuery.getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't get all movies", e);
         }
