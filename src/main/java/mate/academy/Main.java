ada4448f48dbc77b2b48909f074b94fd57ea1108
yuriistorozhuk <yurii.storozhuk@gmail.com>
2023-09-07 07:48:51 +0300

    initial commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         MovieService movieService = null;
 
+
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
