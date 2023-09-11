4748cc201197f3d56ced35450564db2de4d31505
yuriistorozhuk <yurii.storozhuk@gmail.com>
2023-09-11 20:05:12 +0300

    complete<all>: complete hibernate-create-movie-session-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,12 +1,13 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
 import mate.academy.model.Movie;
 import mate.academy.service.MovieService;
 
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService = null;
-
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
---
+++ src/main/java/mate/academy/dao/MovieSessionDao.java
@@ -0,0 +1,17 @@
+package mate.academy.dao;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.model.MovieSession;
+
+public interface MovieSessionDao {
+    MovieSession add(MovieSession movieSession);
+
+    Optional<MovieSession> get(Long id);
+
+    List<MovieSession> getAll();
+
+    List<MovieSession> findAvailableSessions(Long movieId, LocalDateTime start, LocalDateTime end);
+
+}
