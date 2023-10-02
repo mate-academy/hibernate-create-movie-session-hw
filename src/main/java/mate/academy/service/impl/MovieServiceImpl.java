80ad177266ba11dceadef3d7acb0114a9fbe973d
Max Skory <devisrush@gmail.com>
2023-10-02 15:18:53 +0300

    task was implemented

--- src/main/java/mate/academy/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -19,11 +19,12 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie get(Long id) {
-        return movieDao.get(id).get();
+        return movieDao.get(id).orElseThrow(() -> new RuntimeException("There is "
+                + "no Movie by id " + id + " in the database"));
     }
 
     @Override
     public List<Movie> getAll() {
-        return null;
+        return movieDao.getAll();
     }
 }
