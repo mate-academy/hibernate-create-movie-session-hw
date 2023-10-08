a01f5d49f3221831abd0e3bcea600044544d9534
dmytro-voronin <d.voronin.owen@gmail.com>
2023-10-08 16:16:11 +0300

    add new line

--- src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -33,7 +33,9 @@ public class CinemaHallDaoImpl implements CinemaHallDao {
                 session.close();
             }
         }
-    }@Override
+    }
+
+    @Override
     public Optional<CinemaHall> get(Long id) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return Optional.ofNullable(session.get(CinemaHall.class, id));
