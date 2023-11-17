d84a4bd9aeae1ee2c8aa97fd5a7aa31598f15864
Danil Ponomarenko <akame.dante13@gmail.com>
2023-11-17 18:57:01 +0200

    SessionFactories for dao's was created. Renamed variable in HQL query.

--- src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -9,17 +9,19 @@ import mate.academy.lib.Dao;
 import mate.academy.model.MovieSession;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 @Dao
 public class MovieSessionDaoImpl implements MovieSessionDao {
+    private static final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     @Override
     public MovieSession add(MovieSession movieSession) {
         Transaction transaction = null;
         Session session = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             transaction = session.beginTransaction();
             session.persist(movieSession);
             transaction.commit();
@@ -38,7 +40,7 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
 
     @Override
     public Optional<MovieSession> get(Long id) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             return Optional.ofNullable(session.get(MovieSession.class, id));
         } catch (Exception e) {
             throw new DataProcessingException("Can't get a movie session by id: " + id, e);
@@ -47,16 +49,16 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
 
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             Query<MovieSession> getAllMovieSessionsQuery =
                     session.createQuery("from MovieSession ms "
                             + "left join fetch ms.movie "
                             + "left join fetch ms.cinemaHall "
-                            + "where date(ms.showTime) = :day "
+                            + "where date(ms.showTime) = :dayFromDateSqlQuery "
                             + "and ms.showTime >= :startOfDay "
                             + "and ms.showTime <= :endOfDay "
                             + "and ms.id = :movieID", MovieSession.class);
-            getAllMovieSessionsQuery.setParameter("day", date);
+            getAllMovieSessionsQuery.setParameter("dayFromDateSqlQuery", date);
             getAllMovieSessionsQuery.setParameter("startOfDay", date.atStartOfDay());
             getAllMovieSessionsQuery.setParameter("endOfDay", date.atTime(23, 59, 59));
             getAllMovieSessionsQuery.setParameter("movieID", movieId);
