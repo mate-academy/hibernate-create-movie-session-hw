b7c4cbdfed61a6f9f14d85b9fa76ec04ddb5bc86
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-11-17 15:38:27 +0200

    added movie session and cinema hall entities also dao and service layers for them. Implemented all necessary methods above.

---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,62 @@
+package mate.academy.dao.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movie session " + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public MovieSession get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(MovieSession.class, id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get the movie session with given id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> getAllSessionsByDateAndId =
+                    session.createQuery("FROM MovieSession s "
+                                    + "WHERE s.movie.id = :id AND DATE(s.showTime) = DATE(:date)",
+                    MovieSession.class);
+            getAllSessionsByDateAndId.setParameter("id", movieId);
+            getAllSessionsByDateAndId.setParameter("date", date);
+            return getAllSessionsByDateAndId.getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Something went wrong, check id: " + movieId
+                    + " or date " + date, e);
+        }
+    }
+}
