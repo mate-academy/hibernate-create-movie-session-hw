333a36b68f913ed501de3cdf20f7d4598703df23
Taras <tazik1232@gmail.com>
2023-11-19 15:03:02 +0200

    added solution movie-session hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,52 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService = null;
+        Injector injector = Injector.getInstance("mate.academy");
+
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
+        Movie m = new Movie("m");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
+        movieService.add(m);
         System.out.println(movieService.get(fastAndFurious.getId()));
+        System.out.println("getAll method");
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall hall = new CinemaHall();
+        hall.setCapacity(100);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription("Best cinema hall");
+        cinemaHall.setCapacity(500);
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        cinemaHallService.add(cinemaHall);
+        cinemaHallService.add(hall);
+        System.out.println(cinemaHallService.get(cinemaHall.getId()) + "get method");
+        System.out.println("getAll method");
+        cinemaHallService.getAll().forEach(System.out::println);
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(fastAndFurious);
+        movieSession.setCinemaHall(hall);
+        movieSession.setShowTime(LocalDate.of(2023, 11, 19));
+        MovieSessionService movieSessionService =
+                (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+        System.out.println(movieSessionService.get(movieSession.getId()) + "get method");
+        System.out.println("find method");
+        movieSessionService.findAvailableSessions(1L,
+                LocalDate.of(2023, 11, 19)).forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -0,0 +1,64 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movie_sessions")
+public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Movie movie;
+    @ManyToOne
+    private CinemaHall cinemaHall;
+    private LocalDate showTime;
+
+    @Override
+    public String toString() {
+        return "MovieSession{"
+                + "id=" + id
+                + ", movie=" + movie
+                + ", cinemaHall=" + cinemaHall
+                + ", showTime=" + showTime
+                + '}';
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public CinemaHall getCinemaHall() {
+        return cinemaHall;
+    }
+
+    public void setCinemaHall(CinemaHall cinemaHall) {
+        this.cinemaHall = cinemaHall;
+    }
+
+    public LocalDate getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDate showTime) {
+        this.showTime = showTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
