e430a5c459d73b93fae6514b3aab116a1f51fe20
Oleksii <oleksii.makovkin@gmail.com>
2023-12-12 11:54:03 +0200

    fix checkstyle

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -17,19 +17,14 @@ public class Main {
         Injector injector = Injector.getInstance("mate.academy");
         CinemaHallService cinemaHallService =
                 (CinemaHallService) injector.getInstance(CinemaHallService.class);
-
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-
         MovieSessionService movieSessionService =
                 (MovieSessionService) injector.getInstance(MovieSessionService.class);
-
         CinemaHall cinemaHall = new CinemaHall(40, "Cinema Galactic");
         cinemaHallService.add(cinemaHall);
-
         Movie djangoMovie = new Movie("Django Unchained");
         djangoMovie.setDescription("Django Unchained is a 2012 American revisionist Western");
         movieService.add(djangoMovie);
-
         MovieSession movieSession = new MovieSession(djangoMovie, cinemaHall,
                 LocalDateTime.parse("2023-12-12T12:00:00", DateTimeFormatter.ISO_DATE_TIME));
         movieSessionService.add(movieSession);
