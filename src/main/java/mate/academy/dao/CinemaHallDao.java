afc6f47080d8ff2513ba709d2c867e10a6a4098d
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-24 15:55:25 +0200

    added changes to the class CinemaHallDao

--- src/main/java/mate/academy/dao/CinemaHallDao.java
+++ src/main/java/mate/academy/dao/CinemaHallDao.java
@@ -1,13 +1,15 @@
 package mate.academy.dao;
 
+import java.util.List;
+import java.util.Optional;
+import mate.academy.lib.Dao;
 import mate.academy.model.CinemaHall;
 
-import java.util.List;
-
+@Dao
 public interface CinemaHallDao {
     CinemaHall add(CinemaHall cinemaHall);
 
-    CinemaHall get(Long id);
+    Optional<CinemaHall> get(Long id);
 
     List<CinemaHall> getAll();
 }
