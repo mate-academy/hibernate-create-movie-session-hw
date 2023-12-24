237d5ae6c090249dbf09868bee0c1cffd0b073f2
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-24 16:54:29 +0200

    added changes to the class MovieSession

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -1,7 +1,11 @@
 package mate.academy.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
