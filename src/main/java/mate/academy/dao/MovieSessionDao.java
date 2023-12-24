e8d2444acc6f97003ab7c5fb44749bbe80aec67d
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-24 16:20:38 +0200

    added changes to the class MovieSessionDao

--- src/main/java/mate/academy/dao/MovieSessionDao.java
+++ src/main/java/mate/academy/dao/MovieSessionDao.java
@@ -1,14 +1,16 @@
 package mate.academy.dao;
 
-import mate.academy.model.MovieSession;
-
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import mate.academy.lib.Dao;
+import mate.academy.model.MovieSession;
 
+@Dao
 public interface MovieSessionDao {
     MovieSession add(MovieSession movieSession);
 
-    MovieSession get(Long id);
+    Optional<MovieSession> get(Long id);
 
     List<MovieSession> findAvailableSessions(Long movieId, LocalDate date);
 }
