49158b3c14a45187ae4a7969061b967cbde57075
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-24 17:27:05 +0200

    added changes to the class MovieSession

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -1,2 +1,78 @@
-package mate.academy.model;public class MovieSession {
+package mate.academy.model;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movieSessions")
+public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Movie movie;
+    @ManyToOne
+    private CinemaHall cinemaHall;
+    private LocalDate localDate;
+
+    public MovieSession() {
+    }
+
+    public MovieSession(Movie movie, CinemaHall cinemaHall, LocalDate localDate) {
+        this.movie = movie;
+        this.cinemaHall = cinemaHall;
+        this.localDate = localDate;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public CinemaHall getCinemaHall() {
+        return cinemaHall;
+    }
+
+    public void setCinemaHall(CinemaHall cinemaHall) {
+        this.cinemaHall = cinemaHall;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieSession{"
+                +
+                "id=" + id
+                +
+                ", movie=" + movie
+                +
+                ", cinemaHall=" + cinemaHall
+                +
+                ", dateTime=" + localDate
+                +
+                '}';
+    }
 }
