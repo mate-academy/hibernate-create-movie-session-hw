947b78f5f010fcf56fc013678f599234b7070bee
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-24 17:55:54 +0200

    added changes to the class CinemaHallServiceImpl

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -21,7 +21,7 @@ public class CinemaHallServiceImpl implements CinemaHallService {
     @Override
     public CinemaHall get(Long id) {
         return cinemaHallDao.get(id).orElseThrow(()
-                -> new DataProcessingException("No CinemaHall with such id: " + id));
+                -> new DataProcessingException("No CinemaHall with such id " + id));
     }
 
     @Override
