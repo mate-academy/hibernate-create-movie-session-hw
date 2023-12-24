574b523b09fc53792de4d3b3a9001f43caed4fe9
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-24 20:35:03 +0200

    added chanegs to the class CinemaHallDao.java

--- src/main/java/mate/academy/dao/CinemaHallDao.java
+++ src/main/java/mate/academy/dao/CinemaHallDao.java
@@ -2,10 +2,8 @@ package mate.academy.dao;
 
 import java.util.List;
 import java.util.Optional;
-import mate.academy.lib.Dao;
 import mate.academy.model.CinemaHall;
 
-@Dao
 public interface CinemaHallDao {
     CinemaHall add(CinemaHall cinemaHall);
 
