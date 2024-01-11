23cbfff4db4ae32a8273a02c103860b8376a9e9d
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-01-11 20:26:46 +0200

    fixed checkstyle issues

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -2,8 +2,6 @@ package mate.academy.service.impl;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
-
 import mate.academy.dao.CinemaHallDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
