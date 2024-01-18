6f9b1f65f719ce7684b174fb5c51a7296819afaa
LeonIllya <dd0733567@gmail.com>
2024-01-18 12:43:29 +0200

    fix mistakes

--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -1,7 +1,7 @@
 package mate.academy.service.impl;
 
-import java.time.LocalDate;
 import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDate;
 import java.util.List;
 import mate.academy.dao.MovieSessionDao;
 import mate.academy.lib.Inject;
