db76c62b8098e80a0cd25706aae61c5e0e71d96c
Vadim <waterfallkadas@gmail.com>
2024-01-19 19:59:00 +0200

    solution 19/01-4

--- src/main/java/mate/academy/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -1,8 +1,8 @@
 package mate.academy.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import mate.academy.dao.MovieDao;
-import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Movie;
@@ -21,8 +21,8 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Movie get(Long id) {
 
-        return movieDao.get(id).orElseThrow(() -> new DataProcessingException(
-                "Can't get movie from DB by ID " + id, new RuntimeException()));
+        return movieDao.get(id).orElseThrow(() -> new EntityNotFoundException(
+                "Can't get movie from DB by ID " + id));
     }
 
     @Override
