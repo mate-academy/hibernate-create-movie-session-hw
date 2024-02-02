09bf1679714c766e082932116bbacb757a16542a
Vlad Bondarev <bondarev.21@icloud.com>
2024-02-02 13:36:49 +0200

    hm after review

--- src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -2,6 +2,7 @@ package mate.academy.dao.impl;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import mate.academy.dao.MovieSessionDao;
 import mate.academy.exception.DataProcessingException;
@@ -39,9 +40,9 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
     }
 
     @Override
-    public MovieSession get(Long id) {
+    public Optional<MovieSession> get(Long id) {
         try (Session session = SESSION_FACTORY.openSession()) {
-            return session.get(MovieSession.class, id);
+            return Optional.ofNullable(session.get(MovieSession.class, id));
         } catch (Exception e) {
             throw new DataProcessingException("Can't get a movie session by id: " + id, e);
         }
@@ -64,6 +65,9 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
                             movieSession.getShowTime().getDayOfMonth()
                                     == date.getDayOfMonth())
                     .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new DataProcessingException(
+                    "Can't find available sessions by id and date: " + movieId + " " + date, e);
         }
     }
 }
