4c9d80c248142790a9ed9cb654f420d46a9a23c5
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-02-07 15:45:43 +0200

    added hw_solution

---
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -0,0 +1,30 @@
+package mate.academy.service.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.dao.impl.MovieSessionDaoImpl;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.service.MovieSessionService;
+
+@Service
+public class MovieSessionServiceImpl implements MovieSessionService {
+    private final MovieSessionDao movieSessionDao = new MovieSessionDaoImpl();
+
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        return movieSessionDao.add(movieSession);
+    }
+
+    @Override
+    public MovieSession get(Long id) {
+        return movieSessionDao.get(id).get();
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        return movieSessionDao.findAvailableSessions(movieId, date);
+    }
+}
+
