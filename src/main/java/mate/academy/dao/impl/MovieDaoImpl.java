07e5dc94a8c21beb3e9e69e1484786fd1028c45c
AdamMudrak <mudrets2000@gmail.com>
2024-02-22 11:49:46 +0200

    Implement getAll() function

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -45,6 +45,11 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public List<Movie> getAll() {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("SELECT movie FROM Movie movie", Movie.class)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't form a movies list from DB", e);
+        }
     }
 }
