915dd4226e72e901fd8921c6050c0e159ba3319f
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-07 20:45:46 +0300

    created CinemaHallDaoImpl

---
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoIml.java
@@ -0,0 +1,23 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.model.CinemaHall;
+
+import java.util.List;
+
+public class CinemaHallDaoIml implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        return null;
+    }
+
+    @Override
+    public CinemaHall get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        return null;
+    }
+}
