5a7effcbc43801b9011e91a50f0a6cbcbaa3edd6
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-04-07 22:27:32 +0300

    implemented add() get() findAvailableSessions() in MovieSessionServiceImpl

--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -1,5 +1,7 @@
 package mate.academy.service.impl;
 
+import mate.academy.dao.MovieSessionDao;
+import mate.academy.lib.Inject;
 import mate.academy.model.MovieSession;
 import mate.academy.service.MovieSessionService;
 
@@ -7,18 +9,21 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class MovieSessionServiceImpl implements MovieSessionService {
+    @Inject
+    private MovieSessionDao movieSessionDao;
+
     @Override
     public MovieSession add(MovieSession movieSession) {
-        return null;
+        return movieSessionDao.add(movieSession);
     }
 
     @Override
     public MovieSession get(Long id) {
-        return null;
+        return movieSessionDao.get(id).get();
     }
 
     @Override
     public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
-        return null;
+        return movieSessionDao.findAvailableSessions(movieId, date);
     }
 }
