1fdcceda03931eeda5cc39062b283dd3bb6b11c9
Sasha <sasha.panchyshun@gmail.com>
2024-04-12 18:20:58 +0300

    hibernate-create-movie-session-hw-solution 1

--- src/main/java/mate/academy/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieServiceImpl.java
@@ -19,11 +19,12 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie get(Long id) {
-        return movieDao.get(id).get();
+        return movieDao.get(id).orElseThrow(()
+                -> new RuntimeException("Can't get movie by id: " + id));
     }
 
     @Override
     public List<Movie> getAll() {
-        return null;
+        return movieDao.getAll();
     }
 }
