80e6e110968124b0c16ddc43a0b1b833a5edf45b
Nikita <nik.pogrebnoy@gmail.com>
2024-05-03 20:57:17 +0300

    Implemented hibernate-create-movie-session-hw

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,87 @@
 package mate.academy;
 
+import static mate.academy.lib.Injector.getInstance;
+
+import java.time.LocalDate;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
+    public static final Injector injector =
+            getInstance("mate.academy");
+    private static final MovieService movieService =
+            (MovieService) injector.getInstance(MovieService.class);
+    private static final CinemaHallService cinemaHallService =
+            (CinemaHallService) injector.getInstance(CinemaHallService.class);
+    private static final MovieSessionService movieSessionService =
+            (MovieSessionService) injector.getInstance(MovieSessionService.class);
+
     public static void main(String[] args) {
-        MovieService movieService = null;
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
+
+        Movie harryPotter = new Movie("Harry Potter");
+        harryPotter.setDescription("philosopher stone!!!");
+        movieService.add(harryPotter);
+
         System.out.println(movieService.get(fastAndFurious.getId()));
+        System.out.println(movieService.get(harryPotter.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall defaultHall = new CinemaHall();
+        defaultHall.setCapacity(50);
+        defaultHall.setDescription("Default hall");
+
+        CinemaHall luxuryHall = new CinemaHall();
+        luxuryHall.setCapacity(10);
+        luxuryHall.setDescription("Luxury hall");
+
+        cinemaHallService.add(defaultHall);
+        cinemaHallService.add(luxuryHall);
+        System.out.println(cinemaHallService.get(defaultHall.getId()));
+        System.out.println(cinemaHallService.get(luxuryHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSession fastAndFuriousFirstSession = new MovieSession();
+        fastAndFuriousFirstSession.setCinemaHall(defaultHall);
+        fastAndFuriousFirstSession.setDate(LocalDate.of(2024, 5, 14));
+        fastAndFuriousFirstSession.setMovie(fastAndFurious);
+        movieSessionService.add(fastAndFuriousFirstSession);
+
+        MovieSession anotherFastAndFuriousFirstSession = new MovieSession();
+        anotherFastAndFuriousFirstSession.setCinemaHall(luxuryHall);
+        anotherFastAndFuriousFirstSession.setDate(LocalDate.of(2024, 5, 14));
+        anotherFastAndFuriousFirstSession.setMovie(fastAndFurious);
+        movieSessionService.add(anotherFastAndFuriousFirstSession);
+
+        MovieSession fastAndFuriousSecondSession = new MovieSession();
+        fastAndFuriousSecondSession.setCinemaHall(luxuryHall);
+        fastAndFuriousSecondSession.setDate(LocalDate.of(2024, 5, 15));
+        fastAndFuriousSecondSession.setMovie(fastAndFurious);
+        movieSessionService.add(fastAndFuriousSecondSession);
+
+        MovieSession harryPotterFirstSession = new MovieSession();
+        harryPotterFirstSession.setCinemaHall(luxuryHall);
+        harryPotterFirstSession.setDate(LocalDate.of(2024, 6, 1));
+        harryPotterFirstSession.setMovie(harryPotter);
+        movieSessionService.add(harryPotterFirstSession);
+
+        MovieSession harryPotterSecondSession = new MovieSession();
+        harryPotterSecondSession.setCinemaHall(luxuryHall);
+        harryPotterSecondSession.setDate(LocalDate.of(2024, 6, 2));
+        harryPotterSecondSession.setMovie(harryPotter);
+        movieSessionService.add(harryPotterSecondSession);
+
+        movieSessionService
+                .findAvailableSessions(1L,
+                        LocalDate.of(2024, 5, 14))
+                .forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.service.impl;
+
+import java.util.List;
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.service.CinemaHallService;
+
+@Service
+public class CinemaHallServiceImpl implements CinemaHallService {
+    @Inject
+    private CinemaHallDao cinemaHallDao;
+
+    @Override
+    public mate.academy.model.CinemaHall add(mate.academy.model.CinemaHall cinemaHall) {
+        return cinemaHallDao.add(cinemaHall);
+    }
+
+    @Override
+    public mate.academy.model.CinemaHall get(Long id) {
+        return cinemaHallDao.get(id).orElseThrow(() ->
+                new RuntimeException("cannot find cinema hall with id: " + id));
+    }
+
+    @Override
+    public List<mate.academy.model.CinemaHall> getAll() {
+        return cinemaHallDao.getAll();
+    }
+}
