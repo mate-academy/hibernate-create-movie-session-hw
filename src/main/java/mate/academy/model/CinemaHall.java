49820d4a702c03e1b919ede81a6df8a2ce80c601
Vlad <mazafackery@gmail.com>
2024-05-12 15:46:52 +0300

    implemented cinema hall and movie session

--- src/main/java/mate/academy/model/CinemaHall.java
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -7,19 +7,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "movies")
-public class Movie {
+@Table(name = "cinemahalls")
+public class CinemaHall {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
+    private Integer capacity;
     private String description;
 
-    public Movie() {
-    }
+    public CinemaHall() {
 
-    public Movie(String title) {
-        this.title = title;
     }
 
     public Long getId() {
@@ -30,12 +27,12 @@ public class Movie {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public Integer getCapacity() {
+        return capacity;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
     }
 
     public String getDescription() {
@@ -48,12 +45,11 @@ public class Movie {
 
     @Override
     public String toString() {
-        return "Movie{"
+        return "CinemaHall{"
                 + "id="
                 + id
-                + ", title='"
-                + title
-                + '\''
+                + ", capacity="
+                + capacity
                 + ", description='"
                 + description
                 + '\''
