bc3bb6bb9c3916532de3617fa1129011e62f24b7
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-05-16 01:08:13 +0300

    fix checkstyle mistakes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,19 +1,16 @@
 package mate.academy;
 
+import java.time.LocalDateTime;
 import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
 import mate.academy.service.MovieSessionService;
 
-import java.time.LocalDateTime;
-
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
-        MovieSessionService movieSessionService = (MovieSessionService)
-                injector.getInstance(MovieSessionService.class);
         MovieSession movieSession = new MovieSession();
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -24,6 +21,8 @@ public class Main {
         movieSession.setCinemaHall(cinemaHall);
         LocalDateTime localDateTime = LocalDateTime.now();
         movieSession.setShowTime(localDateTime);
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
         movieSessionService.add(movieSession);
 
     }
