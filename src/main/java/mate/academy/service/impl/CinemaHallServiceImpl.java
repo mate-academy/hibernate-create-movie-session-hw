05b37d221815f3dfb35d5ebfe949c225e672aace
Ivan Prystaia <greqit@gmail.com>
2024-06-05 18:46:46 +0300

    use orElseThrow

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -2,6 +2,7 @@ package mate.academy.service.impl;
 
 import java.util.List;
 import mate.academy.dao.CinemaHallDao;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Inject;
 import mate.academy.model.CinemaHall;
 import mate.academy.service.CinemaHallService;
@@ -17,7 +18,9 @@ public class CinemaHallServiceImpl implements CinemaHallService {
 
     @Override
     public CinemaHall get(Long id) {
-        return cinemaHallDao.get(id).get();
+        return cinemaHallDao.get(id)
+                .orElseThrow(() -> new DataProcessingException("CinemaHall with id "
+                        + id + "not found", null));
     }
 
     @Override
--- src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
+++ src/main/java/mate/academy/service/impl/MovieSessionServiceImpl.java
@@ -3,6 +3,7 @@ package mate.academy.service.impl;
 import java.time.LocalDate;
 import java.util.List;
 import mate.academy.dao.MovieSessionDao;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Inject;
 import mate.academy.model.MovieSession;
 import mate.academy.service.MovieSessionService;
@@ -18,7 +19,9 @@ public class MovieSessionServiceImpl implements MovieSessionService {
 
     @Override
     public MovieSession get(Long id) {
-        return movieSessionDao.get(id).get();
+        return movieSessionDao.get(id)
+                .orElseThrow(() -> new DataProcessingException("MovieSessionService with id "
+                        + id + "not found", null));
     }
 
     @Override
