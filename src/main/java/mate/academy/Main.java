b01d930d52ebdbb320f736b08c3867e5b24b6a72
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2024-08-31 19:36:55 +0200

    created hibernate.cfg.xml created CinemaHall and MovieSession classes implemented getAll() in dao and service layers created dao and service layers for CinemaHall,MovieSession

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,48 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final String separator = System.lineSeparator();
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
+    @Inject
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("First Movie");
+        movie.setDescription("Movie with id 1");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        System.out.println(movieService.add(movie));
+        System.out.println(movieService.get(1L));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(65);
+        cinemaHall.setDescription("Cinema Hall with id 1");
+        CinemaHallService cinemaHallService = (CinemaHallService) injector
+                .getInstance(CinemaHallService.class);
+
+        System.out.println(separator + cinemaHallService.add(cinemaHall));
+        System.out.println(cinemaHallService.get(1L));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSession movieSession = new MovieSession();
+        movieSession.setMovie(movie);
+        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setShowTime(LocalDateTime.now());
+        MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
+        System.out.println(separator + movieSessionService.add(movieSession));
+        System.out.println(movieSessionService.get(1L));
+        movieSessionService.findAvailableSession(1L, LocalDate.now()).forEach(System.out::println);
     }
 }
