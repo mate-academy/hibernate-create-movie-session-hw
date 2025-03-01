8bc2737f35652dba93ba43dc0b4c8d676c1a3376
ssx_corp <mr.dimov77@gmail.com>
2025-03-01 14:39:40 +0200

    Changed exception type at getAll()

--- src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieDaoImpl.java
@@ -48,7 +48,7 @@ public class MovieDaoImpl implements MovieDao {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.createQuery("FROM Movie", Movie.class).getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Get all movies failed.", e);
+            throw new DataProcessingException("Get all movies failed.", e);
         }
     }
 }
