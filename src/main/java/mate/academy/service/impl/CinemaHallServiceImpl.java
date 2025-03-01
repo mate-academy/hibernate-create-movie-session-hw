e700451444ec84a4d47e7757734412eeb31240ae
ssx_corp <mr.dimov77@gmail.com>
2025-03-01 14:41:14 +0200

    Checkstyle import changes

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.academy.service.impl;
 
 import java.util.List;
-import java.util.Optional;
 import mate.academy.dao.CinemaHallDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
