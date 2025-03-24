5442f49366c821f455edba48b164612dc770bd92
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2025-03-24 14:07:40 +0100

    first try

---
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,56 @@
+package mate.academy.dao.impl;
+
+import jakarta.persistence.Query;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.MovieSession;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movieSession " + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public MovieSession get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(MovieSession.class, id);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a movieSession by id: " + id, e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("from MovieSession m left join fetch Movie j"
+                + " Where j.id = :movie_id AND m.date BETWEEN"
+                + " :startOfDay AND :endOfDay", MovieSession.class);
+        query.setParameter("movie_id", movieId);
+        query.setParameter("startOfDay", date.atStartOfDay());
+        query.setParameter("endOfDay", date.atTime(LocalTime.MAX));
+        return query.getResultList();
+    }
+}
---
+++ src/main/java/mate/academy/model/CinemaHall.java
@@ -0,0 +1,17 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cinema_hall")
+public class CinemaHall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int capacity;
+    private String description;
+}
