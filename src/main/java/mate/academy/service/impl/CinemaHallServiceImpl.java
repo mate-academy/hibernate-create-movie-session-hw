1a7eca080ab03265d0c5baf444e00dd17c7270a7
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-04-18 14:01:49 +0300

    Adjusted code as per the mentor`s notes

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -21,7 +21,7 @@ public class CinemaHallServiceImpl implements CinemaHallService {
     @Override
     public CinemaHall get(Long id) {
         return cinemaHallDao.get(id).orElseThrow(() ->
-                new EntityNotFoundException("CinemaHall with id " + id + " not found"));
+                new EntityNotFoundException("CinemaHall with id" + id + " not found"));
     }
 
     @Override
